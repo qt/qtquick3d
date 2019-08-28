cafe9ad0b660bbf7516aaa71e91a1380be0edf80
Christian Strømme <christian.stromme@qt.io>
2019-08-28 12:35:19 +0300

    Avoid heap allocation in buildRenderableLayers()
    Change-Id: I5bb2d5b34de3f1dd896aee6c9556338838bf77b3
    Reviewed-by: Andy Nichols <andy.nichols@qt.io>

--- src/runtimerender/rendererimpl/qssgrendererimpl.cpp
+++ src/runtimerender/rendererimpl/qssgrendererimpl.cpp
@@ -127,13 +127,16 @@ static inline QSSGRenderLayer *getNextLayer(QSSGRenderLayer &inLayer)
     return nullptr;
 }
 
-static inline void maybePushLayer(QSSGRenderLayer &inLayer, QVector<QSSGRenderLayer *> &outLayerList)
+// Found by fair roll of the dice (in practice we'll never have more then 1 layer!).
+using QSSGRenderLayerList = QVarLengthArray<QSSGRenderLayer *, 4>;
+
+static inline void maybePushLayer(QSSGRenderLayer &inLayer, QSSGRenderLayerList &outLayerList)
 {
     inLayer.calculateGlobalVariables();
     if (inLayer.flags.testFlag(QSSGRenderLayer::Flag::GloballyActive) && inLayer.flags.testFlag(QSSGRenderLayer::Flag::LayerRenderToTarget))
         outLayerList.push_back(&inLayer);
 }
-static void buildRenderableLayers(QSSGRenderLayer &inLayer, QVector<QSSGRenderLayer *> &renderableLayers, bool inRenderSiblings)
+static void buildRenderableLayers(QSSGRenderLayer &inLayer, QSSGRenderLayerList &renderableLayers, bool inRenderSiblings)
 {
     maybePushLayer(inLayer, renderableLayers);
     if (inRenderSiblings) {
@@ -148,10 +151,8 @@ bool QSSGRendererImpl::prepareLayerForRender(QSSGRenderLayer &inLayer,
                                                const QSSGRenderInstanceId id,
                                                bool forceDirectRender)
 {
-    QVector<QSSGRenderLayer *> renderableLayers;
-    // Found by fair roll of the dice.
-    renderableLayers.reserve(4);
 
+    QSSGRenderLayerList renderableLayers;
     buildRenderableLayers(inLayer, renderableLayers, inRenderSiblings);
 
     bool retval = false;
@@ -182,10 +183,7 @@ void QSSGRendererImpl::renderLayer(QSSGRenderLayer &inLayer,
                                      const QSSGRenderInstanceId id)
 {
     Q_UNUSED(surfaceSize);
-    QVector<QSSGRenderLayer *> renderableLayers;
-    // Found by fair roll of the dice.
-    renderableLayers.reserve(4);
-
+    QSSGRenderLayerList renderableLayers;
     buildRenderableLayers(inLayer, renderableLayers, inRenderSiblings);
 
     const QSSGRef<QSSGRenderContext> &theRenderContext(m_demonContext->renderContext());
