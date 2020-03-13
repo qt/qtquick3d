10b64191695175c71e12a53c5072fe9af1097e18
Inho Lee <inho.lee@qt.io>
2020-03-13 14:35:12 +0100

    Light and Camera : correction of dealing with the direction vector
    Change-Id: I99f73542fe0a697ad12748e3e9452bdaa90da1a3
    Reviewed-by: Andy Nichols <andy.nichols@qt.io>

--- src/plugins/assetimporters/assimp/assimpimporter.cpp
+++ src/plugins/assetimporters/assimp/assimpimporter.cpp
@@ -398,13 +398,13 @@ void AssimpImporter::generateModelProperties(aiNode *modelNode, QTextStream &out
 QSSGQmlUtilities::PropertyMap::Type AssimpImporter::generateLightProperties(aiNode *lightNode, QTextStream &output, int tabLevel)
 {
     aiLight *light = m_lights.value(lightNode);
-    // We assume that the direction vector for a light is (0, 0, 1)
-    // so if the direction vector is non-null, but not (0, 0, 1) we
+    // We assume that the direction vector for a light is (0, 0, -1)
+    // so if the direction vector is non-null, but not (0, 0, -1) we
     // need to correct the translation
     aiMatrix4x4 correctionMatrix;
     if (light->mDirection != aiVector3D(0, 0, 0)) {
-        if (light->mDirection != aiVector3D(0, 0, 1)) {
-            aiMatrix4x4::FromToMatrix(light->mDirection, aiVector3D(0, 0, 1), correctionMatrix);
+        if (light->mDirection != aiVector3D(0, 0, -1)) {
+            aiMatrix4x4::FromToMatrix(light->mDirection, aiVector3D(0, 0, -1), correctionMatrix);
         }
     }
 
@@ -495,10 +495,10 @@ void AssimpImporter::generateCameraProperties(aiNode *cameraNode, QTextStream &o
     // We assume these default forward and up vectors, so if this isn't
     // the case we have to do additional transform
     aiMatrix4x4 correctionMatrix;
-    if (camera->mLookAt != aiVector3D(0, 0, 1))
+    if (camera->mLookAt != aiVector3D(0, 0, -1))
     {
         aiMatrix4x4 lookAtCorrection;
-        aiMatrix4x4::FromToMatrix(camera->mLookAt, aiVector3D(0, 0, 1), lookAtCorrection);
+        aiMatrix4x4::FromToMatrix(camera->mLookAt, aiVector3D(0, 0, -1), lookAtCorrection);
         correctionMatrix *= lookAtCorrection;
     }
 
