3ac066f8e857691050add639b20c2e631f0c565f
Alexandru Croitor <alexandru.croitor@qt.io>
2020-04-02 15:39:07 +0200

    CMake: Handle system assimp correctly
    Make sure to run the config test and only use system assimp
    if the compile tests succeeds.
    
    Make sure to mark the Find module as not found when the system
    assimp library is not found.
    
    Work around the broken assimp Config file which only sets
    IMPORTED_LOCATION_RELEASE on non-Windows platforms, without setting
    IMPORTED_CONFIGURATIONS, which means that if Qt is configured in Debug
    mode, the build will fail due to not finding an appropriate library
    file.
    
    Adapt the config.test to find and link against system assimp.
    
    Change-Id: I05299dce2a5589a95c9300c502fc303ba475a958
    Reviewed-by: Joerg Bornemann <joerg.bornemann@qt.io>

--- cmake/FindWrapAssimp.cmake
+++ cmake/FindWrapAssimp.cmake
@@ -3,10 +3,26 @@ if(TARGET WrapAssimp::WrapAssimp)
   return()
 endif()
 
+set(WrapAssimp_FOUND FALSE)
+
 find_package(assimp ${WrapAssimp_FIND_VERSION} CONFIG QUIET)
 if (assimp_FOUND AND TARGET assimp::assimp)
   add_library(WrapAssimp::WrapAssimp INTERFACE IMPORTED)
   target_link_libraries(WrapAssimp::WrapAssimp INTERFACE assimp::assimp)
+
+  # The Assimp configuration file is broken, and sets only the Release location, without advertising
+  # that it's available via IMPORTED_CONFIGURATIONS. Thus when configuring with a different
+  # configuration, you get a assimp::assimp-NOTFOUND at build time.
+  # Make the release configuration be the default one.
+  get_target_property(_assimp_library_location assimp::assimp IMPORTED_LOCATION)
+  if(NOT _assimp_library_location)
+      get_target_property(_assimp_library_location_release assimp::assimp IMPORTED_LOCATION_RELEASE)
+      if(_assimp_library_location_release)
+          set_target_properties(assimp::assimp PROPERTIES
+                                IMPORTED_LOCATION "${_assimp_library_location_release}")
+      endif()
+  endif()
+
   set(WrapAssimp_FOUND TRUE)
 elseif(assimp_FOUND AND assimp_LIBRARIES AND assimp_INCLUDE_DIRS)
   add_library(WrapAssimp::WrapAssimp INTERFACE IMPORTED)
@@ -14,3 +30,6 @@ elseif(assimp_FOUND AND assimp_LIBRARIES AND assimp_INCLUDE_DIRS)
   target_include_directories(WrapAssimp::WrapAssimp INTERFACE ${assimp_INCLUDE_DIRS})
   set(WrapAssimp_FOUND TRUE)
 endif()
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(WrapAssimp DEFAULT_MSG WrapAssimp_FOUND)
--- src/plugins/assetimporters/configure.cmake
+++ src/plugins/assetimporters/configure.cmake
@@ -12,6 +12,12 @@ set_property(CACHE INPUT_assimp PROPERTY STRINGS undefined no qt system)
 
 qt_find_package(WrapAssimp 5 PROVIDED_TARGETS WrapAssimp::WrapAssimp)
 
+qt_config_compile_test("assimp"
+                   LABEL "Assimp"
+                   PROJECT_PATH "${CMAKE_CURRENT_SOURCE_DIR}/../../../config.tests/assimp"
+                   LIBRARIES WrapAssimp::WrapAssimp
+                   PACKAGES PACKAGE WrapAssimp 5)
+
 
 #### Tests
 
@@ -25,7 +31,7 @@ qt_feature("assimp" PUBLIC PRIVATE
 qt_feature_definition("assimp" "QT_NO_ASSIMP" NEGATE VALUE "1")
 qt_feature("system-assimp" PRIVATE
     LABEL "System Assimp"
-    CONDITION QT_FEATURE_assimp AND WrapAssimp_FOUND
+    CONDITION QT_FEATURE_assimp AND TEST_assimp
     ENABLE INPUT_assimp STREQUAL 'system'
     DISABLE INPUT_assimp STREQUAL 'qt'
 )
