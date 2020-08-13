3b4aaead5340280101b86ebe9bf71b836c86bbcb
Miikka Heikkinen <miikka.heikkinen@qt.io>
2020-08-13 13:11:14 +0300

    Add clear function to Pass.commands property
    Pick-to: 5.15
    Change-Id: I137e4f6688ad30d67fa5911dfe9be215846c3328
    Reviewed-by: Tomi Korpipää <tomi.korpipaa@qt.io>
    Reviewed-by: Laszlo Agocs <laszlo.agocs@qt.io>

--- src/quick3d/qquick3dshaderutils.cpp
+++ src/quick3d/qquick3dshaderutils.cpp
@@ -424,6 +424,12 @@ int QQuick3DShaderUtilsRenderPass::qmlCommandCount(QQmlListProperty<QQuick3DShad
     return that->m_commands.count();
 }
 
+void QQuick3DShaderUtilsRenderPass::qmlCommandClear(QQmlListProperty<QQuick3DShaderUtilsRenderCommand> *list)
+{
+    QQuick3DShaderUtilsRenderPass *that = qobject_cast<QQuick3DShaderUtilsRenderPass *>(list->object);
+    that->m_commands.clear();
+}
+
 QQmlListProperty<QQuick3DShaderUtilsRenderCommand> QQuick3DShaderUtilsRenderPass::commands()
 {
     return QQmlListProperty<QQuick3DShaderUtilsRenderCommand>(this,
@@ -431,7 +437,7 @@ QQmlListProperty<QQuick3DShaderUtilsRenderCommand> QQuick3DShaderUtilsRenderPass
                                                              QQuick3DShaderUtilsRenderPass::qmlAppendCommand,
                                                              QQuick3DShaderUtilsRenderPass::qmlCommandCount,
                                                              QQuick3DShaderUtilsRenderPass::qmlCommandAt,
-                                                             nullptr);
+                                                             QQuick3DShaderUtilsRenderPass::qmlCommandClear);
 }
 
 void QQuick3DShaderUtilsRenderPass::qmlAppendShader(QQmlListProperty<QQuick3DShaderUtilsShader> *list,
