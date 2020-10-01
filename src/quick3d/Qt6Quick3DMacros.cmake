ea0038bc5ca650a661d5949b3608ef09b23875b0
Alexandru Croitor <alexandru.croitor@qt.io>
2020-10-01 15:25:31 +0200

    CMake: Cleanup public API
    Add quotes to file paths.
    Rename function that's probably intended to be internal.
    
    Amends 61ece7b814cfc0dd48e14887a676d3c6e839222f
    
    Task-number: QTBUG-86827
    Change-Id: Ic4b7673630f6caa40b16433ab616499d310e5d63
    Reviewed-by: Cristian Adam <cristian.adam@qt.io>

--- src/quick3d/Qt6Quick3DMacros.cmake
+++ src/quick3d/Qt6Quick3DMacros.cmake
@@ -1,9 +1,9 @@
 
 #quick3d version of qrc to resource
-function(quick3d_generate_resource_from_qrc target qrcfile)
-    set(generatedResourceFile ${CMAKE_CURRENT_BINARY_DIR}/.rcc/generated_${qrcfile}.qrc )
-    set(generatedSourceCode ${CMAKE_CURRENT_BINARY_DIR}/.rcc/qrc_${qrcfile}.cpp )
-    set(rccArgs --name ${qrcfile} --output ${generatedSourceCode}  ${generatedResourceFile})
+function(_qt_internal_quick3d_generate_resource_from_qrc target qrcfile)
+    set(generatedResourceFile "${CMAKE_CURRENT_BINARY_DIR}/.rcc/generated_${qrcfile}.qrc")
+    set(generatedSourceCode "${CMAKE_CURRENT_BINARY_DIR}/.rcc/qrc_${qrcfile}.cpp")
+    set(rccArgs --name "${qrcfile}" --output "${generatedSourceCode}"  "${generatedResourceFile}")
 
     if(NOT QT_FEATURE_zstd)
         list(APPEND rccArgs "--no-zstd")
@@ -11,20 +11,20 @@ function(quick3d_generate_resource_from_qrc target qrcfile)
 
     add_custom_command(
         OUTPUT
-            ${generatedSourceCode}
+            "${generatedSourceCode}"
         COMMAND
             ${QT_CMAKE_EXPORT_NAMESPACE}::rcc
         ARGS
             ${rccArgs}
         DEPENDS
-            ${qrcfile}
+            "${qrcfile}"
             $<TARGET_FILE:${QT_CMAKE_EXPORT_NAMESPACE}::rcc>
             $<TARGET_FILE:${QT_CMAKE_EXPORT_NAMESPACE}::shadergen>
         COMMENT
             RCC ${qrcfile} VERBATIM
     )
 
-    get_target_property(type ${target} TYPE )
+    get_target_property(type ${target} TYPE)
 
     target_sources(${target} PRIVATE "${generatedSourceCode}")
 endfunction()
@@ -36,22 +36,28 @@ function(qt6_quick3d_build_shaders target resource_name)
         "" "PREFIX" "FILES" ${ARGN}
     )
 
-    set(output_qrc generated_${resource_name}.qrc)
-    set(output_dir ${CMAKE_CURRENT_BINARY_DIR}/.rcc)
+    set(output_qrc "generated_${resource_name}.qrc")
+    set(output_dir "${CMAKE_CURRENT_BINARY_DIR}/.rcc")
 
     add_custom_command(
-        OUTPUT ${resource_name}
+        OUTPUT "${resource_name}"
         ${QT_TOOL_PATH_SETUP_COMMAND}
         COMMAND
             ${QT_CMAKE_EXPORT_NAMESPACE}::shadergen
             -g
-            -o ${output_dir}
-            -r ${output_qrc}
-            -s  ${PROJECT_SOURCE_DIR}
+            -o "${output_dir}"
+            -r "${output_qrc}"
+            -s "${PROJECT_SOURCE_DIR}"
             -f \"${arg_FILES}\"
         DEPENDS
             $<TARGET_FILE:${QT_CMAKE_EXPORT_NAMESPACE}::shadergen>
     )
 
-    quick3d_generate_resource_from_qrc(${target} ${resource_name})
+    _qt_internal_quick3d_generate_resource_from_qrc(${target} ${resource_name})
 endfunction()
+
+if(NOT QT_NO_CREATE_VERSIONLESS_FUNCTIONS)
+    function(qt_quick3d_build_shaders)
+        qt6_quick3d_build_shaders(${ARGV})
+    endfunction()
+endif()
