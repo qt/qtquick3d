833568596086e6b1ec0b3a3a8905de3ea6af6d42
Inho Lee <inho.lee@qt.io>
2020-12-17 18:14:13 +0100

    Update Assimp submodule
    Recent Assimp merged a new fix for the morphing animation.
    When a mesh which does not consist of only triangles is converted, a
    assimp postprocessing step destroyed morphing data.
    This update will fix it.
    
    Change-Id: Iadc5a1d5067f5c8b405cdb5a42016418e1335edf
    Reviewed-by: Andy Nichols <andy.nichols@qt.io>

--- src/3rdparty/assimp/revision.h
+++ src/3rdparty/assimp/revision.h
@@ -1,7 +1,7 @@
 #ifndef ASSIMP_REVISION_H_INC
 #define ASSIMP_REVISION_H_INC
 
-#define GitVersion 0x10092cd1
+#define GitVersion 0xb3e1ee3c
 #define GitBranch "HEAD"
 
 #define VER_MAJOR 5
@@ -16,7 +16,7 @@
 #if (GitVersion == 0)
 #define VER_FILEVERSION_STR         STR(VER_MAJOR) "." STR(VER_MINOR) "." STR(VER_PATCH) "." STR(VER_BUILD)
 #else
-#define VER_FILEVERSION_STR         STR(VER_MAJOR) "." STR(VER_MINOR) "." STR(VER_PATCH) "." STR(VER_BUILD) " (Commit 10092cd1)"
+#define VER_FILEVERSION_STR         STR(VER_MAJOR) "." STR(VER_MINOR) "." STR(VER_PATCH) "." STR(VER_BUILD) " (Commit b3e1ee3c)"
 #endif
 
 #ifdef  NDEBUG
