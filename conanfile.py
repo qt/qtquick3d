c689bf8306300b676c44a402eab7384e4423e219
Iikka Eklund <iikka.eklund@qt.io>
2021-06-29 11:31:25 +0300

    Support builds with Conan
    The recipe uses Conan's supported way to inherit from a given
    base class via 'python_requires_extend'.
    
    The qt-conan-common package implements a base class for Qt leaf
    module recipes. The build steps in leaf modules are mostly identical
    so it makes sense to put those in the base class.
    
    Dependencies are read by the base class from the 'dependencies.yaml'
    which is the same file the CI system uses.
    
    Pick-to: 6.2
    Task-number: QTBUG-94851
    Change-Id: Ibecc5a9cd0a6a5e72cca2a2612ff2d455ccfdac2
    Reviewed-by: Jani Heikkinen <jani.heikkinen@qt.io>
    Reviewed-by: Toni Saario <toni.saario@qt.io>

---
+++ conanfile.py
@@ -0,0 +1,59 @@
+#############################################################################
+##
+## Copyright (C) 2021 The Qt Company Ltd.
+## Contact: https://www.qt.io/licensing/
+##
+## This file is part of the release tools of the Qt Toolkit.
+##
+## $QT_BEGIN_LICENSE:GPL-EXCEPT$
+## Commercial License Usage
+## Licensees holding valid commercial Qt licenses may use this file in
+## accordance with the commercial license agreement provided with the
+## Software or, alternatively, in accordance with the terms contained in
+## a written agreement between you and The Qt Company. For licensing terms
+## and conditions see https://www.qt.io/terms-conditions. For further
+## information use the contact form at https://www.qt.io/contact-us.
+##
+## GNU General Public License Usage
+## Alternatively, this file may be used under the terms of the GNU
+## General Public License version 3 as published by the Free Software
+## Foundation with exceptions as appearing in the file LICENSE.GPL3-EXCEPT
+## included in the packaging of this file. Please review the following
+## information to ensure the GNU General Public License requirements will
+## be met: https://www.gnu.org/licenses/gpl-3.0.html.
+##
+## $QT_END_LICENSE$
+##
+#############################################################################
+
+from conans import ConanFile
+import re
+from pathlib import Path
+
+
+def _parse_qt_version_by_key(key: str) -> str:
+    with open(Path(__file__).parent.resolve() / ".cmake.conf") as f:
+        m = re.search(fr'{key} .*"(.*)"', f.read())
+    return m.group(1) if m else ""
+
+
+def _get_qt_minor_version() -> str:
+    return ".".join(_parse_qt_version_by_key("QT_REPO_MODULE_VERSION").split(".")[:2])
+
+
+class QtQuick3D(ConanFile):
+    name = "qtquick3d"
+    license = "GPL-3.0+, Commercial Qt License Agreement"
+    author = "The Qt Company <https://www.qt.io/contact-us>"
+    url = "https://code.qt.io/cgit/qt/qtquick3d.git"
+    description = (
+        "Qt Quick 3D provides a high-level API for creating 3D content "
+        "and 3D user interfaces based on Qt Quick."
+    )
+    topics = "qt", "qt6", "qtquick3d", "3d"
+    settings = "os", "compiler", "arch", "build_type"
+    # for referencing the version number and prerelease tag and dependencies info
+    exports = ".cmake.conf", "dependencies.yaml"
+    exports_sources = "*", "!conan*.*"
+    python_requires = f"qt-conan-common/{_get_qt_minor_version()}@qt/everywhere"
+    python_requires_extend = "qt-conan-common.QtLeafModule"
