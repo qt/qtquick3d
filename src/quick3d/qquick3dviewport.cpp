c3479841cd4569c27168cf41a355063197025695
Shawn Rutledge <shawn.rutledge@qt.io>
2021-09-20 07:08:07 +0200

    Add check for null subscene root item in QQuick3DViewport::internalPick
    Codechecker found a case where it could be null.
    https://testresults.qt.io/codechecker/daily_diffs/qtquick3d/dev/qtquick3d-dev-20210920-7885b834da/
    
    Change-Id: I0fd957f25244f821fab02f074902ede4b6b15aa4
    Reviewed-by: Laszlo Agocs <laszlo.agocs@qt.io>
    (cherry picked from commit 6447bd3661473843124230c65086c927e099aa37)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/quick3d/qquick3dviewport.cpp
+++ src/quick3d/qquick3dviewport.cpp
@@ -1038,7 +1038,7 @@ bool QQuick3DViewport::internalPick(QPointerEvent *event, const QVector3D &origi
                 auto item2D = qobject_cast<QQuick3DItem2D *>(frontendObject);
                 if (item2D)
                     subsceneRootItem = item2D->contentItem();
-                if (subsceneRootItem->childItems().isEmpty())
+                if (!subsceneRootItem || subsceneRootItem->childItems().isEmpty())
                     continue; // ignore empty 2D subscenes
                 // In this case the "UV" coordinates are in pixels in the subscene root item, so we can just use them.
                 subscenePosition = pickResult.m_localUVCoords.toPointF();
