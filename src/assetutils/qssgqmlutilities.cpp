47b1f31616372a73fee9ed4e249e402f2391796b
Volker Hilsheimer <volker.hilsheimer@qt.io>
2022-01-07 09:46:46 +0100

    Fix compiler warning, parameter might be unused
    Change-Id: I8a92ec1e1943c431eaf6e12f42b936a14a386f65
    Reviewed-by: Christian Strømme <christian.stromme@qt.io>
    (cherry picked from commit fa6496f6bcd0ccce3a6fe0378843432acb63b311)

--- src/assetutils/qssgqmlutilities.cpp
+++ src/assetutils/qssgqmlutilities.cpp
@@ -1328,6 +1328,7 @@ void createTimelineAnimation(const QSSGSceneDesc::Animation &anim, QObject *pare
 #else // QT_QUICK3D_ENABLE_RT_ANIMATIONS
     Q_UNUSED(anim)
     Q_UNUSED(parent)
+    Q_UNUSED(isEnabled)
     Q_UNUSED(useBinaryKeyframes)
 #endif // QT_QUICK3D_ENABLE_RT_ANIMATIONS
 }
