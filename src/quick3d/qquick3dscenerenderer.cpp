3d93b4c180980cad9ebe54df5d1f6dae9d965039
Antti Määttä <antti.maatta@qt.io>
2022-03-29 09:34:02 +0300

    Clear layer resources when scene gets destroyed
    Fix memory leak in layer resources when the same window and render context
    is used to load and render multiple scenes and then deleted. The layer
    resources do not get destroyed when the scene is destroyed so add new
    function to release them.
    
    Fixes: QTBUG-97714
    Change-Id: I643f4912869a39657a696026fddb0c5b9fe6b902
    Reviewed-by: Laszlo Agocs <laszlo.agocs@qt.io>

--- src/quick3d/qquick3dscenerenderer.cpp
+++ src/quick3d/qquick3dscenerenderer.cpp
@@ -540,6 +540,7 @@ void QQuick3DSceneRenderer::addNodeToLayer(QSSGRenderNode *node)
 void QQuick3DSceneRenderer::deleteResources()
 {
     m_sceneManager->updateDirtyNodes();
+    m_sgContext->deleteLayerResources(*m_layer);
     delete m_layer;
     delete m_fbo;
     delete m_antialiasingFbo;
