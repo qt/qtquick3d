aa7ccd09478e714398a97f8f1a15c3c7df598999
Andy Nichols <nezticle@gmail.com>
2022-05-11 10:13:07 +0200

    Assimp: Update to version 5.2.3
    Change-Id: I14f3df1c4417351a5523afda5158274621880b17
    Reviewed-by: Qt CI Bot <qt_ci_bot@qt-project.org>
    Reviewed-by: Christian Strømme <christian.stromme@qt.io>

--- src/3rdparty/assimp/revision.h
+++ src/3rdparty/assimp/revision.h
@@ -1,12 +1,12 @@
 #ifndef ASSIMP_REVISION_H_INC
 #define ASSIMP_REVISION_H_INC
 
-#define GitVersion 0x8134cae9
+#define GitVersion 0x19f2a624
 #define GitBranch "HEAD"
 
 #define VER_MAJOR 5
-#define VER_MINOR 1
-#define VER_PATCH 6
+#define VER_MINOR 2
+#define VER_PATCH 3
 #define VER_BUILD 0
 
 #define STR_HELP(x) #x
@@ -16,7 +16,7 @@
 #if (GitVersion == 0)
 #define VER_FILEVERSION_STR         STR(VER_MAJOR) "." STR(VER_MINOR) "." STR(VER_PATCH) "." STR(VER_BUILD)
 #else
-#define VER_FILEVERSION_STR         STR(VER_MAJOR) "." STR(VER_MINOR) "." STR(VER_PATCH) "." STR(VER_BUILD) " (Commit 8134cae9)"
+#define VER_FILEVERSION_STR         STR(VER_MAJOR) "." STR(VER_MINOR) "." STR(VER_PATCH) "." STR(VER_BUILD) " (Commit 19f2a624)"
 #endif
 
 #ifdef  NDEBUG
