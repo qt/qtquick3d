e979268dc38edd3c5f438d696adbbf19c6ecd088
Inho Lee <inho.lee@qt.io>
2022-09-22 13:00:56 +0200

    Item2D : Delete the RP descriptor when cleanup
    Item2D rendernode has its own renderpass descriptor and
    the deletion should be done before deleting the rhi context.
    
    Fixes: QTBUG-96302
    Change-Id: I328850dd139117d24a67f1aeff17efe4d522b851
    Reviewed-by: Jonas Karlsson <jonas.karlsson@qt.io>
    (cherry picked from commit fba78e59e95347b50e49e0c02782d55f7dac45aa)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/runtimerender/graphobjects/qssgrenderitem2d.cpp
+++ src/runtimerender/graphobjects/qssgrenderitem2d.cpp
@@ -15,6 +15,10 @@ QSSGRenderItem2D::QSSGRenderItem2D()
 
 QSSGRenderItem2D::~QSSGRenderItem2D()
 {
+    // Normally it will be deleted when m_renderer destroyed
+    // by QQuick3DItem2D's connection
+    // But if this backend node may suddenly be deleted,
+    // it is safe to remain this deletion here.
     delete m_rp;
 }
 
