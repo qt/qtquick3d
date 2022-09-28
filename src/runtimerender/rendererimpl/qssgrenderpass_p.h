b2a976dd41482324aabb725ac4584fcbd70d410f
Alexey Edelev <alexey.edelev@qt.io>
2022-09-28 13:39:36 +0200

    Add the missing 'We mean it' warning
    Task-number: QTBUG-87480
    Change-Id: Ia6815a81300c4c7149c1effb55574c02064ee568
    Reviewed-by: Alexandru Croitor <alexandru.croitor@qt.io>

--- src/runtimerender/rendererimpl/qssgrenderpass_p.h
+++ src/runtimerender/rendererimpl/qssgrenderpass_p.h
@@ -4,6 +4,17 @@
 #ifndef QSSGRENDERPASS_H
 #define QSSGRENDERPASS_H
 
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qt API.  It exists purely as an
+// implementation detail.  This header file may change from version to
+// version without notice, or even be removed.
+//
+// We mean it.
+//
+
 #include <QtCore/qglobal.h>
 
 #include <QtQuick3DUtils/private/qtquick3dutilsglobal_p.h>
