b7939f6da1981ac14e67f2f44eba5fbf86bf00b8
Antti Määttä <antti.maatta@qt.io>
2022-10-20 14:25:28 +0300

    Prevent unnecessary memory allocation per frame
    The texture size by of QSGPlainTexture is (-1, -1) by default and it
    is not set when calling setTexture. It is used to compare current
    render size to previous frame texture size, which always fails and
    new QSGPLainTexture gets allocated every time. Set the texture size
    manually so that it gets compared to the correct size.
    
    Change-Id: I661a83d90e30489e86cdb05e404e14778d944209
    Reviewed-by: Tomi Korpipää <tomi.korpipaa@qt.io>
    Reviewed-by: Laszlo Agocs <laszlo.agocs@qt.io>
    (cherry picked from commit 85a253d0fe4fa236ee02c1f5fd0770937fa6d330)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/quick3d/qquick3dscenerenderer.cpp
+++ src/quick3d/qquick3dscenerenderer.cpp
@@ -117,6 +117,7 @@ void SGFramebufferObjectNode::render()
                 t->setOwnsTexture(false);
                 t->setHasAlphaChannel(true);
                 t->setTexture(rhiTexture);
+                t->setTextureSize(renderer->surfaceSize());
                 setTexture(t);
             }
         }
