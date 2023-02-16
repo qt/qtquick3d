qt/qtquick3d

commit 44eb903894ccc11da94dcfa71278e89466d2ca10

AssetImporter: Avoid converting back and forth between QJsonObject and QVarianMap
Change-Id: Ic124b9c2293c7b8b10499c0caa61f3ad7fd45f9a
Reviewed-by: Andy Nichols <andy.nichols@qt.io>

tools/balsamui/settingstab.cpp
@@ -41,41 +41,45 @@ SettingsTab::SettingsTab(QWidget *parent) : QScrollArea(parent)
     const auto allDescs = QSSGAssetImportManager().getImporterPluginInfos();
 
     for (const auto &plugin : allDescs) {
-        auto options = plugin.importOptions.value("options").toMap();
-
+        auto options = plugin.importOptions;
         // Skip plugins without options
-        if (options.empty())
+        if (options.isEmpty())
             continue;
+
+        if (auto optionsIt = options.constFind(QLatin1String("options")); optionsIt != options.constEnd())
+            options = optionsIt->toObject();
 
         QGroupBox *pluginGroup = new QGroupBox(plugin.typeDescription + " (" + plugin.name + ")");
         QFormLayout *extensionsLayout = new QFormLayout;
         extensionsLayout->addRow(new QLabel(tr("Supported extensions:")), new QLabel(plugin.inputExtensions.join(", ")));
 
-        for (auto kv_it = options.constKeyValueBegin(); kv_it != options.constKeyValueEnd(); ++kv_it) {
-            auto map = kv_it->second.toMap();
-            auto description = map.value("description");
-            auto name = map.value("name");
-            auto type = map.value("type");
-            auto value = map.value("value");
+        for (auto kv_it = options.constBegin(); kv_it != options.constEnd(); ++kv_it) {
+            if (kv_it->isObject()) {
+                auto map = kv_it->toObject();
+                auto description = map.value("description");
+                auto name = map.value(QLatin1String("name"));
+                auto type = map.value("type");
+                auto value = map.value("value");
 
-            auto label = new QLabel(name.toString() + ":");
-            label->setToolTip(description.toString());
+                auto label = new QLabel(name.toString() + ":");
+                label->setToolTip(description.toString());
 
-            if (type == "Boolean") {
-                QCheckBox *checkBox = new QCheckBox();
-                checkBox->setChecked(value.toBool());
-                extensionsLayout->addRow(label, checkBox);
-                settings.push_back(Setting { checkBox, kv_it->first, value.toBool(), 0.0f });
-            } else if (type == "Real") {
-                QSpinBox *spinBox = new QSpinBox();
-                spinBox->setMinimum(-9999);
-                spinBox->setMaximum(9999);
-                spinBox->setSingleStep(1);
-                spinBox->setValue(value.toReal());
-                extensionsLayout->addRow(label, spinBox);
-                settings.push_back(Setting { spinBox, kv_it->first, false, value.toReal() });
-            } else {
-                qWarning() << "Unsupported setting " << name;
+                if (type == "Boolean") {
+                    QCheckBox *checkBox = new QCheckBox();
+                    checkBox->setChecked(value.toBool());
+                    extensionsLayout->addRow(label, checkBox);
+                    settings.push_back(Setting { checkBox, name.toString(), value.toBool(), 0.0f });
+                } else if (type == "Real") {
+                    QSpinBox *spinBox = new QSpinBox();
+                    spinBox->setMinimum(-9999);
+                    spinBox->setMaximum(9999);
+                    spinBox->setSingleStep(1);
+                    spinBox->setValue(value.toDouble());
+                    extensionsLayout->addRow(label, spinBox);
+                    settings.push_back(Setting { spinBox, name.toString(), false, value.toDouble() });
+                } else {
+                    qWarning() << "Unsupported setting " << name;
+                }
             }
         }
 
@@ -89,18 +93,18 @@ SettingsTab::SettingsTab(QWidget *parent) : QScrollArea(parent)
     setWidgetResizable(true);
 }
 
-QVariantMap SettingsTab::getOptions() const
+QJsonObject SettingsTab::getOptions() const
 {
-    QVariantMap options;
+    QJsonObject options;
 
     for (const Setting &setting : settings) {
         auto checkBox = dynamic_cast<QCheckBox *>(setting.uiELement);
         auto spinBox = dynamic_cast<QSpinBox *>(setting.uiELement);
 
         if (checkBox != nullptr && setting.defaultBool != checkBox->isChecked()) {
-            options[setting.name] = QVariant::fromValue(checkBox->isChecked());
+            options[setting.name] = QJsonValue(checkBox->isChecked());
         } else if (spinBox != nullptr && setting.defaultReal != spinBox->value()) {
-            options[setting.name] = QVariant::fromValue(double(spinBox->value()));
+            options[setting.name] = QJsonValue(double(spinBox->value()));
         }
     }
 

src/assetimport/qssgassetimporter_p.h
@@ -47,6 +47,7 @@
 #include <QtCore/QObject>
 #include <QtCore/QDir>
 #include <QtCore/QFile>
+#include <QtCore/qjsonobject.h>
 
 QT_BEGIN_NAMESPACE
 
@@ -64,14 +65,14 @@ public:
     virtual QStringList inputExtensions() const = 0;
     virtual QString outputExtension() const = 0;
     virtual QString type() const = 0;
-    virtual QVariantMap importOptions() const = 0;
+    virtual QJsonObject importOptions() const = 0;
     virtual QString typeDescription() const = 0;
     virtual QString import(const QString &sourceFile,
                            const QDir &savePath,
-                           const QVariantMap &options,
+                           const QJsonObject &options,
                            QStringList *generatedFiles = nullptr) = 0;
     virtual QString import(const QUrl &url,
-                           const QVariantMap &options,
+                           const QJsonObject &options,
                            QSSGSceneDesc::Scene &scene) = 0;
 };
 

src/assetimport/qssgassetimportmanager.cpp
@@ -71,12 +71,12 @@ QSSGAssetImportManager::ImportState QSSGAssetImportManager::importFile(const QSt
                                                                        const QDir &outputPath,
                                                                        QString *error)
 {
-    return importFile(filename, outputPath, QVariantMap(), error);
+    return importFile(filename, outputPath, QJsonObject(), error);
 }
 
 QSSGAssetImportManager::ImportState QSSGAssetImportManager::importFile(const QString &filename,
                                                                        const QDir &outputPath,
-                                                                       const QVariantMap &options,
+                                                                       const QJsonObject &options,
                                                                        QString *error)
 {
     QFileInfo fileInfo(filename);
@@ -128,7 +128,7 @@ QSSGAssetImportManager::ImportState QSSGAssetImportManager::importFile(const QUr
 
     if (it != end) {
         const auto &importer = *it;
-        const auto ret = importer->import(url, QVariantMap(), scene);
+        const auto ret = importer->import(url, QJsonObject(), scene);
         if (!ret.isEmpty()) {
             if (error)
                 *error = ret;
@@ -139,11 +139,11 @@ QSSGAssetImportManager::ImportState QSSGAssetImportManager::importFile(const QUr
     return ImportState::Success;
 }
 
-QVariantMap QSSGAssetImportManager::getOptionsForFile(const QString &filename)
+QJsonObject QSSGAssetImportManager::getOptionsForFile(const QString &filename)
 {
     QFileInfo fileInfo(filename);
 
-    QVariantMap options;
+    QJsonObject options;
 
     // Is this a real file?
     if (fileInfo.exists()) {
@@ -157,9 +157,9 @@ QVariantMap QSSGAssetImportManager::getOptionsForFile(const QString &filename)
     return options;
 }
 
-QHash<QString, QVariantMap> QSSGAssetImportManager::getAllOptions() const
+QSSGAssetImportManager::PluginOptionMaps QSSGAssetImportManager::getAllOptions() const
 {
-    QHash<QString, QVariantMap> options;
+    PluginOptionMaps options;
     for (const auto importer : m_assetImporters)
         options.insert(importer->inputExtensions().join(QChar::fromLatin1(':')), importer->importOptions());
     return options;

src/assetimport/qssgassetimportmanager_p.h
@@ -47,9 +47,9 @@
 #include <QtCore/QVector>
 #include <QtCore/QMap>
 #include <QtCore/QDir>
-#include <QtCore/QVariant>
 #include <QtCore/QString>
 #include <QtCore/QList>
+#include <QtCore/qjsonobject.h>
 
 QT_BEGIN_NAMESPACE
 
@@ -63,7 +63,7 @@ struct QSSGAssetImporterPluginInfo
     QStringList inputExtensions;
     QString outputExtension;
     QString type;
-    QVariantMap importOptions;
+    QJsonObject importOptions;
     QString typeDescription;
 };
 
@@ -81,19 +81,21 @@ public:
         Unsupported
     };
 
+    using PluginOptionMaps = QHash<QString, QJsonObject>;
+
     // ### Temp API
     ImportState importFile(const QString &filename,
                            const QDir &outputPath,
                            QString *error = nullptr);
     ImportState importFile(const QString &filename,
                            const QDir &outputPath,
-                           const QVariantMap &options = QVariantMap(),
+                           const QJsonObject &options = QJsonObject(),
                            QString *error = nullptr);
     ImportState importFile(const QUrl &url,
                            QSSGSceneDesc::Scene &scene,
                            QString *error = nullptr);
-    QVariantMap getOptionsForFile(const QString &filename);
-    QHash<QString, QVariantMap> getAllOptions() const;
+    QJsonObject getOptionsForFile(const QString &filename);
+    PluginOptionMaps getAllOptions() const;
     QHash<QString, QStringList> getSupportedExtensions() const;
     QList<QSSGAssetImporterPluginInfo> getImporterPluginInfos() const;
 

src/plugins/assetimporters/assimp/assimpimporter.cpp
@@ -123,7 +123,7 @@ AssimpImporter::AssimpImporter()
     QByteArray options = optionFile.readAll();
     optionFile.close();
     auto optionsDocument = QJsonDocument::fromJson(options);
-    m_options = optionsDocument.object().toVariantMap();
+    m_options = optionsDocument.object();
     m_postProcessSteps = aiPostProcessSteps(demonPostProcessPresets);
 
     m_importer = new Assimp::Importer();
@@ -170,7 +170,7 @@ QString AssimpImporter::typeDescription() const
     return QObject::tr("3D Scene");
 }
 
-QVariantMap AssimpImporter::importOptions() const
+QJsonObject AssimpImporter::importOptions() const
 {
     return m_options;
 }
@@ -190,10 +190,8 @@ bool fuzzyCompare(const aiQuaternion &q1, const aiQuaternion &q2)
 }
 }
 
-QString AssimpImporter::import(const QString &sourceFile, const QDir &savePath, const QVariantMap &options, QStringList *generatedFiles)
+QString AssimpImporter::import(const QString &sourceFile, const QDir &savePath, const QJsonObject &options, QStringList *generatedFiles)
 {
-    Q_UNUSED(options);
-
     QString errorString;
     m_savePath = savePath;
     m_sourceFile = QFileInfo(sourceFile);
@@ -2016,97 +2014,96 @@ QString AssimpImporter::generateUniqueId(const QString &id)
     return uniqueID;
 }
 
-void AssimpImporter::processOptions(const QVariantMap &options)
+void AssimpImporter::processOptions(QJsonObject options)
 {
     // Setup import settings based given options
     // You can either pass the whole options object, or just the "options" object
     // so get the right scope.
-    QJsonObject optionsObject = QJsonObject::fromVariantMap(options);
-    if (optionsObject.contains(QStringLiteral("options")))
-        optionsObject = optionsObject.value(QStringLiteral("options")).toObject();
+    if (auto it = options.constFind("options"), end = options.constEnd(); it != end)
+        options = it->toObject();
 
-    if (optionsObject.isEmpty())
+    if (options.isEmpty())
         return;
 
     // parse the options list for values
     // We always need to triangulate and remove non triangles
     m_postProcessSteps = aiPostProcessSteps(aiProcess_Triangulate | aiProcess_SortByPType);
 
-    if (checkBooleanOption(QStringLiteral("calculateTangentSpace"), optionsObject))
+    if (checkBooleanOption(QStringLiteral("calculateTangentSpace"), options))
         m_postProcessSteps = aiPostProcessSteps(m_postProcessSteps | aiProcess_CalcTangentSpace);
 
-    if (checkBooleanOption(QStringLiteral("joinIdenticalVertices"), optionsObject))
+    if (checkBooleanOption(QStringLiteral("joinIdenticalVertices"), options))
         m_postProcessSteps = aiPostProcessSteps(m_postProcessSteps | aiProcess_JoinIdenticalVertices);
 
-    if (checkBooleanOption(QStringLiteral("generateNormals"), optionsObject))
+    if (checkBooleanOption(QStringLiteral("generateNormals"), options))
         m_postProcessSteps = aiPostProcessSteps(m_postProcessSteps | aiProcess_GenNormals);
 
-    if (checkBooleanOption(QStringLiteral("generateSmoothNormals"), optionsObject))
+    if (checkBooleanOption(QStringLiteral("generateSmoothNormals"), options))
         m_postProcessSteps = aiPostProcessSteps(m_postProcessSteps | aiProcess_GenSmoothNormals);
 
-    if (checkBooleanOption(QStringLiteral("splitLargeMeshes"), optionsObject))
+    if (checkBooleanOption(QStringLiteral("splitLargeMeshes"), options))
         m_postProcessSteps = aiPostProcessSteps(m_postProcessSteps | aiProcess_SplitLargeMeshes);
 
-    if (checkBooleanOption(QStringLiteral("preTransformVertices"), optionsObject))
+    if (checkBooleanOption(QStringLiteral("preTransformVertices"), options))
         m_postProcessSteps = aiPostProcessSteps(m_postProcessSteps | aiProcess_PreTransformVertices);
 
-    if (checkBooleanOption(QStringLiteral("improveCacheLocality"), optionsObject))
+    if (checkBooleanOption(QStringLiteral("improveCacheLocality"), options))
         m_postProcessSteps = aiPostProcessSteps(m_postProcessSteps | aiProcess_ImproveCacheLocality);
 
-    if (checkBooleanOption(QStringLiteral("removeRedundantMaterials"), optionsObject))
+    if (checkBooleanOption(QStringLiteral("removeRedundantMaterials"), options))
         m_postProcessSteps = aiPostProcessSteps(m_postProcessSteps | aiProcess_RemoveRedundantMaterials);
 
-    if (checkBooleanOption(QStringLiteral("fixInfacingNormals"), optionsObject))
+    if (checkBooleanOption(QStringLiteral("fixInfacingNormals"), options))
         m_postProcessSteps = aiPostProcessSteps(m_postProcessSteps | aiProcess_FixInfacingNormals);
 
-    if (checkBooleanOption(QStringLiteral("findDegenerates"), optionsObject))
+    if (checkBooleanOption(QStringLiteral("findDegenerates"), options))
         m_postProcessSteps = aiPostProcessSteps(m_postProcessSteps | aiProcess_FindDegenerates);
 
-    if (checkBooleanOption(QStringLiteral("findInvalidData"), optionsObject))
+    if (checkBooleanOption(QStringLiteral("findInvalidData"), options))
         m_postProcessSteps = aiPostProcessSteps(m_postProcessSteps | aiProcess_FindInvalidData);
 
-    if (checkBooleanOption(QStringLiteral("transformUVCoordinates"), optionsObject))
+    if (checkBooleanOption(QStringLiteral("transformUVCoordinates"), options))
         m_postProcessSteps = aiPostProcessSteps(m_postProcessSteps | aiProcess_TransformUVCoords);
 
-    if (checkBooleanOption(QStringLiteral("findInstances"), optionsObject))
+    if (checkBooleanOption(QStringLiteral("findInstances"), options))
         m_postProcessSteps = aiPostProcessSteps(m_postProcessSteps | aiProcess_FindInstances);
 
-    if (checkBooleanOption(QStringLiteral("optimizeMeshes"), optionsObject))
+    if (checkBooleanOption(QStringLiteral("optimizeMeshes"), options))
         m_postProcessSteps = aiPostProcessSteps(m_postProcessSteps | aiProcess_OptimizeMeshes);
 
-    if (checkBooleanOption(QStringLiteral("optimizeGraph"), optionsObject))
+    if (checkBooleanOption(QStringLiteral("optimizeGraph"), options))
         m_postProcessSteps = aiPostProcessSteps(m_postProcessSteps | aiProcess_OptimizeGraph);
 
-    if (checkBooleanOption(QStringLiteral("globalScale"), optionsObject)) {
-        m_globalScaleValue = getRealOption(QStringLiteral("globalScaleValue"), optionsObject);
+    if (checkBooleanOption(QStringLiteral("globalScale"), options)) {
+        m_globalScaleValue = getRealOption(QStringLiteral("globalScaleValue"), options);
         if (m_globalScaleValue == 0.0)
             m_globalScaleValue = 1.0;
     }
 
-    if (checkBooleanOption(QStringLiteral("dropNormals"), optionsObject))
+    if (checkBooleanOption(QStringLiteral("dropNormals"), options))
         m_postProcessSteps = aiPostProcessSteps(m_postProcessSteps | aiProcess_DropNormals);
 
     aiComponent removeComponents = aiComponent(0);
 
-    if (checkBooleanOption(QStringLiteral("removeComponentNormals"), optionsObject))
+    if (checkBooleanOption(QStringLiteral("removeComponentNormals"), options))
         removeComponents = aiComponent(removeComponents | aiComponent_NORMALS);
 
-    if (checkBooleanOption(QStringLiteral("removeComponentTangentsAndBitangents"), optionsObject))
+    if (checkBooleanOption(QStringLiteral("removeComponentTangentsAndBitangents"), options))
         removeComponents = aiComponent(removeComponents | aiComponent_TANGENTS_AND_BITANGENTS);
 
-    if (checkBooleanOption(QStringLiteral("removeComponentColors"), optionsObject))
+    if (checkBooleanOption(QStringLiteral("removeComponentColors"), options))
         removeComponents = aiComponent(removeComponents | aiComponent_COLORS);
 
-    if (checkBooleanOption(QStringLiteral("removeComponentUVs"), optionsObject))
+    if (checkBooleanOption(QStringLiteral("removeComponentUVs"), options))
         removeComponents = aiComponent(removeComponents | aiComponent_TEXCOORDS);
 
-    if (checkBooleanOption(QStringLiteral("removeComponentBoneWeights"), optionsObject))
+    if (checkBooleanOption(QStringLiteral("removeComponentBoneWeights"), options))
         removeComponents = aiComponent(removeComponents | aiComponent_BONEWEIGHTS);
 
-    if (checkBooleanOption(QStringLiteral("removeComponentAnimations"), optionsObject))
+    if (checkBooleanOption(QStringLiteral("removeComponentAnimations"), options))
         removeComponents = aiComponent(removeComponents | aiComponent_ANIMATIONS);
 
-    if (checkBooleanOption(QStringLiteral("removeComponentTextures"), optionsObject))
+    if (checkBooleanOption(QStringLiteral("removeComponentTextures"), options))
         removeComponents = aiComponent(removeComponents | aiComponent_TEXTURES);
 
     if (removeComponents != aiComponent(0)) {
@@ -2114,32 +2111,28 @@ void AssimpImporter::processOptions(const QVariantMap &options)
         m_importer->SetPropertyInteger(AI_CONFIG_PP_RVC_FLAGS, removeComponents);
     }
 
-    bool preservePivots = checkBooleanOption(QStringLiteral("fbxPreservePivots"), optionsObject);
+    bool preservePivots = checkBooleanOption(QStringLiteral("fbxPreservePivots"), options);
     m_importer->SetPropertyBool(AI_CONFIG_IMPORT_FBX_PRESERVE_PIVOTS, preservePivots);
 
-    m_useFloatJointIndices = checkBooleanOption(QStringLiteral("useFloatJointIndices"), optionsObject);
-    m_forceMipMapGeneration = checkBooleanOption(QStringLiteral("generateMipMaps"), optionsObject);
-    m_binaryKeyframes = checkBooleanOption(QStringLiteral("useBinaryKeyframes"), optionsObject);
+    m_useFloatJointIndices = checkBooleanOption(QStringLiteral("useFloatJointIndices"), options);
+    m_forceMipMapGeneration = checkBooleanOption(QStringLiteral("generateMipMaps"), options);
+    m_binaryKeyframes = checkBooleanOption(QStringLiteral("useBinaryKeyframes"), options);
 
-    m_generateLightmapUV = checkBooleanOption(QStringLiteral("generateLightmapUV"), optionsObject);
+    m_generateLightmapUV = checkBooleanOption(QStringLiteral("generateLightmapUV"), options);
 }
 
 bool AssimpImporter::checkBooleanOption(const QString &optionName, const QJsonObject &options)
 {
-    if (!options.contains(optionName))
-        return false;
-
-    QJsonObject option = options.value(optionName).toObject();
-    return option.value(QStringLiteral("value")).toBool();
+    const auto it = options.constFind(optionName);
+    const auto end = options.constEnd();
+    return (it != end) ? it->toObject().value("value").toBool() : false;
 }
 
 qreal AssimpImporter::getRealOption(const QString &optionName, const QJsonObject &options)
 {
-    if (!options.contains(optionName))
-        return false;
-
-    QJsonObject option = options.value(optionName).toObject();
-    return option.value(QStringLiteral("value")).toDouble();
+    const auto it = options.constFind(optionName);
+    const auto end = options.constEnd();
+    return (it != end) ? it->toObject().value("value").toDouble() : 0.0;
 }
 
 QT_END_NAMESPACE

src/plugins/assetimporters/assimp/assimpimporter.h
@@ -42,6 +42,7 @@
 #include <QtCore/QSet>
 #include <QtCore/QVariant>
 #include <QtCore/QCborStreamWriter>
+#include <QtCore/qjsonobject.h>
 
 #include "assimputils.h"
 
@@ -85,10 +86,10 @@ public:
     QString outputExtension() const override;
     QString type() const override;
     QString typeDescription() const override;
-    QVariantMap importOptions() const override;
-    QString import(const QString &sourceFile, const QDir &savePath, const QVariantMap &options,
-                         QStringList *generatedFiles) override;
-    QString import(const QUrl &sourceFile, const QVariantMap &options, QSSGSceneDesc::Scene &scene) override;
+    QJsonObject importOptions() const override;
+    QString import(const QString &sourceFile, const QDir &savePath, const QJsonObject &options,
+                   QStringList *generatedFiles) override;
+    QString import(const QUrl &sourceFile, const QJsonObject &options, QSSGSceneDesc::Scene &scene) override;
 
 private:
     void writeHeader(QTextStream &output);
@@ -120,7 +121,7 @@ private:
     bool isCamera(aiNode *node);
     bool isBone(aiNode *node);
     QString generateUniqueId(const QString &id);
-    void processOptions(const QVariantMap &options);
+    void processOptions(QJsonObject options);
     bool checkBooleanOption(const QString &optionName, const QJsonObject &options);
     qreal getRealOption(const QString &optionName, const QJsonObject &options);
 
@@ -153,7 +154,7 @@ private:
     bool m_generateLightmapUV = false;
     qreal m_globalScaleValue = 1.0;
 
-    QVariantMap m_options;
+    QJsonObject m_options;
     aiPostProcessSteps m_postProcessSteps;
 
 };

src/plugins/assetimporters/assimp/assimpimporter_rt.cpp
@@ -1487,7 +1487,7 @@ static QString importImp(const QUrl &url, const QVariantMap &options, QSSGSceneD
 
 ////////////////////////
 
-QString AssimpImporter::import(const QUrl &url, const QVariantMap &, QSSGSceneDesc::Scene &scene)
+QString AssimpImporter::import(const QUrl &url, const QJsonObject &, QSSGSceneDesc::Scene &scene)
 {
     // We'll simply use assimp to load the scene and then translate the Aassimp scene
     // into our own format.

tools/balsam/main.cpp
@@ -50,9 +50,8 @@ public:
         qDeleteAll(m_optionsMap);
         m_optionsMap.clear();
     }
-    void generateCommandLineOptions(const QVariantMap &optionsMap)
+    void generateCommandLineOptions(const QJsonObject &options)
     {
-        QJsonObject options = QJsonObject::fromVariantMap(optionsMap);
         if (options.isEmpty() || !options.contains(QStringLiteral("options")))
             return;
 
@@ -77,11 +76,10 @@ public:
         }
     }
 
-    QVariantMap processCommandLineOptions(const QCommandLineParser &cmdLineParser, const QVariantMap &optionsMap) const
+    QJsonObject processCommandLineOptions(const QCommandLineParser &cmdLineParser, const QJsonObject &options) const
     {
-        QJsonObject options = QJsonObject::fromVariantMap(optionsMap);
         if (options.isEmpty() || !options.contains(QStringLiteral("options")))
-            return optionsMap;
+            return options;
 
         QJsonObject optionsObject = options.value(QStringLiteral("options")).toObject();
         for (const QString &optionsKey : optionsObject.keys()) {
@@ -103,7 +101,7 @@ public:
 
         removeFlagConflicts(cmdLineParser, optionsObject);
         options["options"] = optionsObject;
-        return optionsObject.toVariantMap();
+        return optionsObject;
     }
     void registerOptions(QCommandLineParser &parser) {
         for (const auto &cmdLineOption : qAsConst(m_optionsMap))
@@ -250,7 +248,7 @@ int main(int argc, char *argv[])
         QString errorString;
         QSSGAssetImportManager::ImportState result = QSSGAssetImportManager::ImportState::Unsupported;
         if (canUsePlugins) {
-            QVariantMap options = assetImporter->getOptionsForFile(assetFileName);
+            QJsonObject options = assetImporter->getOptionsForFile(assetFileName);
             options = optionsManager.processCommandLineOptions(cmdLineParser, options);
             // first try the plugin-based asset importer system
             result = assetImporter->importFile(assetFileName, outputDirectory, options, &errorString);

commit 043745c912341e4205854a42065a7987b3acb06d

Fix setting wrong option key in the balsamui
This fixes bug introduced in 44eb90389

Taks-number: QTBUG-111000
Pick-to: 6.5 6.4
Change-Id: I639f1ee2c0e882d898e339ab61d73c235ec5b87d
Reviewed-by: Kristoffer Skau <kristoffer.skau@qt.io>
Reviewed-by: Andy Nichols <andy.nichols@qt.io>

tools/balsamui/settingstab.cpp
@@ -30,6 +30,7 @@ SettingsTab::SettingsTab(QWidget *parent) : QScrollArea(parent)
         for (auto kv_it = options.constBegin(); kv_it != options.constEnd(); ++kv_it) {
             if (kv_it->isObject()) {
                 auto map = kv_it->toObject();
+                auto settingKeyName = kv_it.key();
                 auto description = map.value("description");
                 auto name = map.value(QLatin1String("name"));
                 auto type = map.value("type");
@@ -42,7 +43,7 @@ SettingsTab::SettingsTab(QWidget *parent) : QScrollArea(parent)
                     QCheckBox *checkBox = new QCheckBox();
                     checkBox->setChecked(value.toBool());
                     extensionsLayout->addRow(label, checkBox);
-                    settings.push_back(Setting { checkBox, name.toString(), value.toBool(), 0.0f });
+                    settings.push_back(Setting { checkBox, settingKeyName, value.toBool(), 0.0f });
                 } else if (type == "Real") {
                     QSpinBox *spinBox = new QSpinBox();
                     spinBox->setMinimum(-9999);
@@ -50,7 +51,7 @@ SettingsTab::SettingsTab(QWidget *parent) : QScrollArea(parent)
                     spinBox->setSingleStep(1);
                     spinBox->setValue(value.toDouble());
                     extensionsLayout->addRow(label, spinBox);
-                    settings.push_back(Setting { spinBox, name.toString(), false, value.toDouble() });
+                    settings.push_back(Setting { spinBox, settingKeyName, false, value.toDouble() });
                 } else {
                     qWarning() << "Unsupported setting " << name;
                 }