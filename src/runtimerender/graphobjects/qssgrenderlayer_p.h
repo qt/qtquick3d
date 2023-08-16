08447fb972743a74963ac1fbddcaf6ec5ee42744
Christian Strømme <christian.stromme@qt.io>
2023-08-16 12:50:03 +0200

    Remove unused member from the render layer
    Change-Id: Ie1228eac12d4d71b5a34bf4b3da8855272dbc3a9
    Reviewed-by: Andy Nichols <andy.nichols@qt.io>
    (cherry picked from commit 2445306c7b604766986e7149cb680967944fd328)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/runtimerender/graphobjects/qssgrenderlayer_p.h
+++ src/runtimerender/graphobjects/qssgrenderlayer_p.h
@@ -119,13 +119,6 @@ struct Q_QUICK3DRUNTIMERENDER_EXPORT QSSGRenderLayer : public QSSGRenderNode
     QSSGRenderEffect *firstEffect;
     QSSGLayerRenderData *renderData = nullptr;
 
-    // If a layer has a valid texture path (one that resolves to either a
-    // an on-disk image or a offscreen renderer), then it does not render its
-    // own source path.  Instead, it renders the offscreen renderer.  Used in this manner,
-    // offscreen renderer's also have the option (if they support it) to render directly to the
-    // render target given a specific viewport (that is also scissored if necessary).
-    QString texturePath;
-
     QSSGRenderLayer::AAMode antialiasingMode;
     QSSGRenderLayer::AAQuality antialiasingQuality;
 
