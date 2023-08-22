4affc27ebee4383be0aab30ab1b7935e6c1038c7
Christian Strømme <christian.stromme@qt.io>
2023-08-22 15:14:00 +0200

    Add a debug condition to qssgassert
    The QSSG_DEBUG_COND macro makes it possible to make an conditional into
    an almost noop for release builds. This makes it possible to have more
    expensive sanity checks that only gets executed in debug builds
    
    Change-Id: I351b2a509fc2b1bc41be545f31637ded14607759
    Reviewed-by: Andy Nichols <andy.nichols@qt.io>
    (cherry picked from commit b2e766d3c999321efe5906aa5aeab64414170fd0)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/utils/qssgassert.cpp
+++ src/utils/qssgassert.cpp
@@ -116,6 +116,27 @@ QT_BEGIN_NAMESPACE
     Same as \l QSSG_GUARD() but with a custom \a message that will be print if \a condition is not met.
 */
 
+/*!
+    \macro QSSG_DEBUG_COND(condition)
+    \internal
+
+    Macro for condition that should only be run in debug builds. In releases build the macro
+    produces an "almost-no-op" condition (always true) and the \a condition is never run.
+    Can e.g., be combined with the assert checks to add potentially expensive sanity checks
+    that should only be run in debug builds.
+
+    \badcode
+    QSSG_CEHCK(QSSG_DEBUG_COND(!list.contains(...)));
+    \endcode
+
+    In a release build the \c QSSG_DEBUG_COND will never return \c false and the \a condition will never
+    be evaluated.
+
+    \note DO NOT make surrounding code depend on the \a condition being evaluated or called!.
+
+    \note Unlike the assert checks, this macro does not change behavior in relation to developer-builds.
+*/
+
 void qssgWriteAssertLocation(const char *msg)
 {
 #if defined(QT_BUILD_INTERNAL)
