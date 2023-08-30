3df8515ab9b9128515f821813f47ffb39c53e2dd
Andy Nichols <andy.nichols@qt.io>
2023-08-30 15:24:25 +0200

    Add missing QML_ADDED_IN_VERSION(6, 6) for new APIs
    Pick-to: 6.6
    Change-Id: I9756d7218f1ca5075649c9adc5f8a81dea973346
    Reviewed-by: Christian Strømme <christian.stromme@qt.io>

--- src/quick3d/extensions/qquick3drenderextensions_p.h
+++ src/quick3d/extensions/qquick3drenderextensions_p.h
@@ -29,6 +29,7 @@ class Q_QUICK3D_EXPORT QQuick3DRenderExtension : public QQuick3DObject
     Q_OBJECT
     QML_NAMED_ELEMENT(RenderExtension)
     QML_UNCREATABLE("RenderExtension is an abstract type")
+    QML_ADDED_IN_VERSION(6, 6)
 public:
     explicit QQuick3DRenderExtension(QQuick3DObject *parent = nullptr);
     virtual ~QQuick3DRenderExtension();
