qt/qtquick3d

commit 1bca206a3516875896b92508b0de9dcdaae7dfba

Extensions API
Add extension API making for doing application side extensions.

Change-Id: I5d496709aa28787a8c0c82f3e016b92c600107c9
Reviewed-by: Andy Nichols <andy.nichols@qt.io>

src/runtimerender/rendererimpl/qssglayerrenderdata_p.h
@@ -28,6 +28,7 @@
 #include <QtQuick3DRuntimeRender/private/qssgrenderreflectionmap_p.h>
 #include <QtQuick3DRuntimeRender/private/qssgrendercamera_p.h>
 #include <QtQuick3DRuntimeRender/private/qssgrhicontext_p.h>
+#include <QtQuick3DRuntimeRender/private/qssgrenderextensions_p.h>
 
 #include <QtQuick3DUtils/private/qssgrenderbasetypes_p.h>
 
@@ -123,17 +124,11 @@ struct QSSGLayerRenderPreparationResultFlags : public QFlags<QSSGLayerRenderPrep
     }
 };
 
-struct QSSGCameraData
+struct QSSGCameraRenderData
 {
-    QSSGCameraData() = default;
-    QSSGCameraData(const QVector3D &dir, const QVector3D &pos)
-        : direction(dir)
-        , position(pos)
-    {}
-    QSSGCameraData(const QSSGRenderCamera &camera)
-        : QSSGCameraData(camera.getScalingCorrectDirection(), camera.getGlobalPos())
-    {}
-    QVector3D direction;
+    QMatrix4x4 viewProjection;
+    std::optional<QSSGClippingFrustum> clippingFrustum;
+    QVector3D direction { 0.0f, 0.0f, -1.0f };
     QVector3D position;
 };
 
@@ -149,15 +144,6 @@ struct QSSGLayerRenderPreparationResult
     bool isLayerVisible() const;
     QSize textureDimensions() const;
     QSSGCameraGlobalCalculationResult setupCameraForRender(QSSGRenderCamera &inCamera);
-};
-
-struct QSSGRenderableNodeEntry
-{
-    QSSGRenderNode *node = nullptr;
-    mutable QSSGRenderMesh *mesh = nullptr;
-    mutable QSSGShaderLightListView lights;
-    QSSGRenderableNodeEntry() = default;
-    QSSGRenderableNodeEntry(QSSGRenderNode &inNode) : node(&inNode) {}
 };
 
 struct QSSGDefaultMaterialPreparationResult
@@ -190,6 +176,8 @@ public:
         MAX_TEMPORAL_AA_LEVELS = 2,
     };
 
+    using RenderableFilter = std::function<bool(QSSGModelContext *)>;
+
     QSSGLayerRenderData(QSSGRenderLayer &inLayer, QSSGRenderer &inRenderer);
     ~QSSGLayerRenderData();
 
@@ -212,36 +200,23 @@ public:
                                 QSSGShaderDefaultMaterialKey &key,
                                 QSSGRenderer *renderer);
 
-    QSSGDefaultMaterialPreparationResult prepareDefaultMaterialForRender(QSSGRenderDefaultMaterial &inMaterial,
-                                                                         QSSGRenderableObjectFlags &inExistingFlags,
-                                                                         float inOpacity,
-                                                                         const QSSGShaderLightListView &lights,
-                                                                         QSSGLayerRenderPreparationResultFlags &ioFlags);
-
-    QSSGDefaultMaterialPreparationResult prepareCustomMaterialForRender(QSSGRenderCustomMaterial &inMaterial,
-                                                                        QSSGRenderableObjectFlags &inExistingFlags,
-                                                                        float inOpacity, bool alreadyDirty,
-                                                                        const QSSGShaderLightListView &lights,
-                                                                        QSSGLayerRenderPreparationResultFlags &ioFlags);
-
     // Load meshes as needed
-    static void prepareModelMeshesForRender(const QSSGRenderContextInterface &contextInterface,
-                                            RenderableNodeEntries &renderableModels,
-                                            bool globalPickingEnabled);
+    static void prepareModelMeshes(const QSSGRenderContextInterface &contextInterface,
+                                   RenderableNodeEntries &renderableModels);
 
     static void prepareModelBoneTextures(const QSSGRenderContextInterface &contextInterface,
                                          const RenderableNodeEntries &renderableModels);
 
     // Helper functions used during PrepareForRender and PrepareAndRender
     // Updates lights with model receivesShadows. Do not pass globalLights.
-    bool prepareModelForRender(const RenderableNodeEntries &renderableModels,
-                               const QMatrix4x4 &inViewProjection,
-                               QSSGLayerRenderPreparationResultFlags &ioFlags,
-                               const QSSGCameraData &cameraData,
-                               float lodThreshold = 0.0f);
-    bool prepareParticlesForRender(const RenderableNodeEntries &renderableParticles, const QSSGCameraData &cameraData);
-    static bool prepareItem2DsForRender(const QSSGRenderContextInterface &ctxIfc, const RenderableItem2DEntries &renderableItem2Ds,
-                                        const QMatrix4x4 &inViewProjection);
+    bool prepareModelsForRender(const RenderableNodeEntries &renderableModels,
+                                QSSGLayerRenderPreparationResultFlags &ioFlags,
+                                const QSSGCameraRenderData &cameraData,
+                                RenderableFilter filter,
+                                float lodThreshold = 0.0f);
+    bool prepareParticlesForRender(const RenderableNodeEntries &renderableParticles, const QSSGCameraRenderData &cameraData);
+    bool prepareItem2DsForRender(const QSSGRenderContextInterface &ctxIfc,
+                                 const RenderableItem2DEntries &renderableItem2Ds);
 
     void prepareResourceLoaders();
 
@@ -252,7 +227,7 @@ public:
     static qsizetype frustumCulling(const QSSGClippingFrustum &clipFrustum, const QSSGRenderableObjectList &renderables, QSSGRenderableObjectList &visibleRenderables);
     [[nodiscard]] static qsizetype frustumCullingInline(const QSSGClippingFrustum &clipFrustum, QSSGRenderableObjectList &renderables);
 
-    [[nodiscard]] QSSGCameraData getCameraDirectionAndPosition();
+
     // Per-frame cache of renderable objects post-sort (for the MAIN rendering camera, i.e., don't use these lists for rendering from a different camera).
     const QSSGRenderableObjectList &getSortedOpaqueRenderableObjects();
     // If layer depth test is false, this may also contain opaque objects.
@@ -267,16 +242,20 @@ public:
 
     void maybeBakeLightmap();
 
+    QSSGFrameData &getFrameData();
+
     ShadowMapPass shadowMapPass;
     ReflectionMapPass reflectionMapPass;
     ZPrePassPass zPrePassPass;
     SSAOMapPass ssaoMapPass;
     DepthMapPass depthMapPass;
     ScreenMapPass screenMapPass;
+    static constexpr size_t USERPASSES = 2; // See QSSGRenderLayer::RenderExtensionMode::Count
+    UserPass userPasses[USERPASSES];
     MainPass mainPass;
 
     // Built-in passes
-    QVarLengthArray<QSSGRenderPass *, 8> activePasses;
+    QVarLengthArray<QSSGRenderPass *, 12> activePasses;
 
     QSSGRenderLayer &layer;
     QSSGRenderer *renderer = nullptr;
@@ -310,7 +289,7 @@ public:
 
     std::optional<QSSGClippingFrustum> clippingFrustum;
     std::optional<QSSGLayerRenderPreparationResult> layerPrepResult;
-    std::optional<QSSGCameraData> cameraData;
+    std::optional<QSSGCameraRenderData> cameraData;
 
     TModelContextPtrList modelContexts;
 
@@ -328,6 +307,21 @@ public:
 
     bool plainSkyBoxPrepared = false;
 
+    [[nodiscard]] QSSGRenderableNodeEntry getNode(QSSGNodeId id) const;
+    [[nodiscard]] QSSGRenderableNodeEntry takeNode(QSSGNodeId id);
+
+    [[nodiscard]] QSSGRenderGraphObject *getResource(QSSGResourceId id) const;
+
+    [[nodiscard]] QSSGRenderCamera *activeCamera() const { return camera; }
+
+    [[nodiscard]] QSSGCameraRenderData getCameraRenderData(const QSSGRenderCamera *camera);
+    [[nodiscard]] QSSGCameraRenderData getCameraRenderData(const QSSGRenderCamera *camera) const;
+
+    void setLightmapTexture(const QSSGModelContext &modelContext, QRhiTexture *lightmapTexture);
+    [[nodiscard]] QRhiTexture *getLightmapTexture(const QSSGModelContext &modelContext) const;
+
+    [[nodiscard]] QSSGRenderContextInterface *contextInterface() const;
+
     // Temp. API. Ideally there shouldn't be a reason for anyone to hold onto these,
     // but we follow the existing pattern for now.
     const QSSGRenderShadowMapPtr &requestShadowMapManager();
@@ -336,7 +330,34 @@ public:
     const QSSGRenderReflectionMapPtr &getReflectionMapManager() const { return reflectionMapManager; }
 
 private:
+    friend class QSSGRenderer;
+    friend class QSSGFrameData;
+    friend class QSSGModelHelpers;
+    friend class QSSGRenderHelpers;
+
+    [[nodiscard]] QSSGCameraRenderData getCachedCameraData();
     void updateSortedDepthObjectsListImp();
+
+    [[nodiscard]] static QSSGLayerRenderData *getCurrent(const QSSGRenderer &renderer) { return renderer.m_currentLayer; }
+
+    QSSGDefaultMaterialPreparationResult prepareDefaultMaterialForRender(QSSGRenderDefaultMaterial &inMaterial,
+                                                                         QSSGRenderableObjectFlags &inExistingFlags,
+                                                                         float inOpacity,
+                                                                         const QSSGShaderLightListView &lights,
+                                                                         QSSGLayerRenderPreparationResultFlags &ioFlags);
+
+    QSSGDefaultMaterialPreparationResult prepareCustomMaterialForRender(QSSGRenderCustomMaterial &inMaterial,
+                                                                        QSSGRenderableObjectFlags &inExistingFlags,
+                                                                        float inOpacity, bool alreadyDirty,
+                                                                        const QSSGShaderLightListView &lights,
+                                                                        QSSGLayerRenderPreparationResultFlags &ioFlags);
+
+
+    static void prepareModelMeshesForRenderInternal(const QSSGRenderContextInterface &contextInterface,
+                                                    RenderableNodeEntries &renderableModels,
+                                                    bool globalPickingEnabled);
+
+    std::optional<QSSGFrameData> frameData;
     QSSGRhiGraphicsPipelineState ps; // Base pipleline state
     QSSGShaderFeatures features; // Base feature set
     bool particlesEnabled = true;

src/runtimerender/rendererimpl/qssgrenderpass.cpp
@@ -6,11 +6,14 @@
 #include "qssglayerrenderdata_p.h"
 #include "qssgrendercontextcore_p.h"
 #include "qssgdebugdrawsystem_p.h"
+#include "extensionapi/qssgrenderextensions_p.h"
 
 #include "../utils/qssgassert_p.h"
 
 #include <QtQuick/private/qsgrenderer_p.h>
 #include <qtquick3d_tracepoints_p.h>
+
+#include <QtQuick3D/QQuick3DObject>
 
 QT_BEGIN_NAMESPACE
 
@@ -899,6 +902,33 @@ void MainPass::release()
     sortedTransparentObjects.clear();
     sortedScreenTextureObjects.clear();
     item2Ds.clear();
+}
+
+void UserPass::renderPrep(QSSGRenderer &renderer, QSSGLayerRenderData &data)
+{
+    auto &frameData = data.getFrameData();
+    for (const auto &p : std::as_const(extensions)) {
+        p->prepareRender(renderer, frameData);
+        if (p->type() == QSSGRenderExtension::Type::Standalone)
+            p->render(renderer);
+    }
+}
+
+void UserPass::renderPass(QSSGRenderer &renderer)
+{
+    for (const auto &p : std::as_const(extensions)) {
+        if (p->type() == QSSGRenderExtension::Type::Main)
+            p->render(renderer);
+    }
+}
+
+void UserPass::release()
+{
+    for (const auto &p : std::as_const(extensions))
+        p->release();
+
+    // TODO: We should track if we need to update this list.
+    extensions.clear();
 }
 
 QT_END_NAMESPACE

src/quick3d/CMakeLists.txt
@@ -69,6 +69,8 @@ qt_internal_add_qml_module(Quick3D
         qquick3dspecularglossymaterial.cpp qquick3dspecularglossymaterial_p.h
         qquick3dfog.cpp qquick3dfog_p.h
         qquick3dlightmapbaker.cpp qquick3dlightmapbaker_p.h
+        extensions/qquick3drenderextensions.cpp extensions/qquick3drenderextensions_p.h
+        extensions/qquick3dextensionhelpers.cpp extensions/qquick3dextensionhelpers_p.h
     DEFINES
         QT_BUILD_QUICK3D_LIB
     LIBRARIES

src/quick3d/extensions/qquick3dextensionhelpers.cpp
@@ -1,0 +1,50 @@
+// Copyright (C) 2023 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only
+
+#include "qquick3dextensionhelpers_p.h"
+#include "qquick3dobject_p.h"
+
+QT_BEGIN_NAMESPACE
+
+/*!
+    \class QQuick3DExtensionHelpers
+    \inmodule QtQuick3D
+    \since 6.6
+
+    \brief Helper functions for the Extensions APIs.
+*/
+
+QQuick3DExtensionHelpers::QQuick3DExtensionHelpers()
+{
+
+}
+
+/*!
+    \fn QQuick3DExtensionHelpers::getNodeId(const QQuick3DObject &node)
+    \a node a QQuick3D node object.
+    \return a \c QSSGNodeId that can be used to retrieve the corresponding \a {node}{node's} object
+    in the engine.
+
+    \sa QSSGFrameData::getNode()
+*/
+QSSGNodeId QQuick3DExtensionHelpers::getNodeId(const QQuick3DObject &node)
+{
+    // NOTE: Implementation detail (don't rely on this in user code).
+    return quintptr(QQuick3DObjectPrivate::get(&node)->spatialNode);
+}
+
+/*!
+    \fn QQuick3DExtensionHelpers::getResourceId(const QQuick3DObject &resource)
+    \a resource a QQuick3D resource object.
+    \return a \c QSSGResourceId that can be used to retrieve the corresponding \a resource object
+    in the engine.
+
+    \sa QSSGFrameData::getResource()
+*/
+QSSGResourceId QQuick3DExtensionHelpers::getResourceId(const QQuick3DObject &resource)
+{
+    // NOTE: Implementation detail (don't rely on this in user code).
+    return quintptr(QQuick3DObjectPrivate::get(&resource)->spatialNode);
+}
+
+QT_END_NAMESPACE

src/quick3d/extensions/qquick3dextensionhelpers_p.h
@@ -1,0 +1,38 @@
+// Copyright (C) 2023 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only
+
+#ifndef QQUICK3DEXTENSIONHELPERS_P_H
+#define QQUICK3DEXTENSIONHELPERS_P_H
+
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qt API.  It exists purely as an
+// implementation detail.  This header file may change from version to
+// version without notice, or even be removed.
+//
+// We mean it.
+//
+
+#include <QtQuick3D/qtquick3dglobal.h>
+
+QT_BEGIN_NAMESPACE
+
+using QSSGNodeId = quintptr;
+using QSSGResourceId = quintptr;
+
+class QQuick3DObject;
+
+class Q_QUICK3D_EXPORT QQuick3DExtensionHelpers
+{
+public:
+    QQuick3DExtensionHelpers();
+
+    [[nodiscard]] static QSSGNodeId getNodeId(const QQuick3DObject &node);
+    [[nodiscard]] static QSSGResourceId getResourceId(const QQuick3DObject &resource);
+};
+
+QT_END_NAMESPACE
+
+#endif // QQUICK3DEXTENSIONHELPERS_P_H

src/quick3d/extensions/qquick3drenderextensions.cpp
@@ -1,0 +1,31 @@
+// Copyright (C) 2023 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only
+
+#include "qquick3drenderextensions_p.h"
+
+#include <QtQuick3D/private/qquick3dobject_p.h>
+
+QT_BEGIN_NAMESPACE
+
+/*!
+    \class QQuick3DRenderExtension
+    \inmodule QtQuick3D
+    \since 6.6
+
+    \brief Abstract class for implementing user side render extensions.
+
+    \sa QSSGRenderExtension
+*/
+
+QQuick3DRenderExtension::QQuick3DRenderExtension(QQuick3DObject *parent)
+    : QQuick3DObject(*new QQuick3DObjectPrivate(QQuick3DObjectPrivate::Type::RenderExtension), parent)
+{
+
+}
+
+QQuick3DRenderExtension::~QQuick3DRenderExtension()
+{
+
+}
+
+QT_END_NAMESPACE

src/quick3d/extensions/qquick3drenderextensions_p.h
@@ -1,0 +1,39 @@
+// Copyright (C) 2023 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only
+
+#ifndef QQUICK3DRENDEREXTENSIONS_P_H
+#define QQUICK3DRENDEREXTENSIONS_P_H
+
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qt API.  It exists purely as an
+// implementation detail.  This header file may change from version to
+// version without notice, or even be removed.
+//
+// We mean it.
+//
+
+#include <QtCore/qobject.h>
+#include <QtQuick3D/qquick3dobject.h>
+#include <QtQuick3DRuntimeRender/private/qssgrenderextensions_p.h>
+
+QT_BEGIN_NAMESPACE
+
+class QSSGRenderer;
+class QSSGLayerRenderData;
+
+class Q_QUICK3D_EXPORT QQuick3DRenderExtension : public QQuick3DObject
+{
+    Q_OBJECT
+    QML_NAMED_ELEMENT(RenderExtension)
+    QML_UNCREATABLE("RenderExtension is an abstract type")
+public:
+    explicit QQuick3DRenderExtension(QQuick3DObject *parent = nullptr);
+    virtual ~QQuick3DRenderExtension();
+};
+
+QT_END_NAMESPACE
+
+#endif // QQUICK3DRENDEREXTENSIONS_P_H

commit 5df183549216d8b262cbf5a73fa952b089a33622

Fix frustum culling
The frustum culling was using the wrong camera data.
This issue was introduced in 1bca206a3. Added a simple lancelot test to
verify that culling is done.

Pick-to: 6.6
Fixes: QTBUG-117619
Change-Id: I2962bc1292ddd80c05ca6855960b23250bd6efd3
Reviewed-by: Andy Nichols <andy.nichols@qt.io>

src/runtimerender/rendererimpl/qssglayerrenderdata.cpp
@@ -2101,7 +2101,6 @@ void QSSGLayerRenderData::resetForFrame()
     // to figure out if this layer was rendered at all.
     camera = nullptr;
     cameraData.reset();
-    clippingFrustum.reset();
     renderedOpaqueObjects.clear();
     renderedTransparentObjects.clear();
     renderedScreenTextureObjects.clear();

src/runtimerender/rendererimpl/qssglayerrenderdata_p.h
@@ -295,7 +295,6 @@ public:
     QVector<QSSGBakedLightingModel> renderedBakedLightingModels;
     RenderableItem2DEntries renderedItem2Ds;
 
-    std::optional<QSSGClippingFrustum> clippingFrustum;
     QSSGLayerRenderPreparationResult layerPrepResult;
     std::optional<QSSGCameraRenderData> cameraData;
 

src/runtimerender/rendererimpl/qssgrenderpass.cpp
@@ -585,12 +585,13 @@ void OpaquePass::renderPrep(QSSGRenderer &renderer, QSSGLayerRenderData &data)
     const auto &rhiCtx = renderer.contextInterface()->rhiContext();
     QSSG_ASSERT(rhiCtx->rhi()->isRecordingFrame(), return);
     auto camera = data.camera;
-    QSSG_ASSERT(camera, return);
+    const auto &cameraData = data.cameraData;
+    QSSG_ASSERT(camera && cameraData.has_value() , return);
 
     ps = data.getPipelineState();
 
     { // Frustum culling
-        const auto &clippingFrustum = data.clippingFrustum;
+        const auto &clippingFrustum = data.cameraData->clippingFrustum;
         const auto &opaqueObjects = data.getSortedOpaqueRenderableObjects();
         if (clippingFrustum.has_value())
             QSSGLayerRenderData::frustumCulling(clippingFrustum.value(), opaqueObjects, sortedOpaqueObjects);
@@ -652,7 +653,8 @@ void TransparentPass::renderPrep(QSSGRenderer &renderer, QSSGLayerRenderData &da
     const auto &rhiCtx = renderer.contextInterface()->rhiContext();
     QSSG_ASSERT(rhiCtx->rhi()->isRecordingFrame(), return);
     auto camera = data.camera;
-    QSSG_ASSERT(camera, return);
+    const auto &cameraData = data.cameraData;
+    QSSG_ASSERT(camera && cameraData.has_value(), return);
 
     QRhiRenderPassDescriptor *mainRpDesc = rhiCtx->mainRenderPassDescriptor();
     const int samples = rhiCtx->mainPassSampleCount();
@@ -666,7 +668,7 @@ void TransparentPass::renderPrep(QSSGRenderer &renderer, QSSGLayerRenderData &da
     shaderFeatures = data.getShaderFeatures();
 
     { // Frustum culling
-        const auto &clippingFrustum = data.clippingFrustum;
+        const auto &clippingFrustum = data.cameraData->clippingFrustum;
         const auto &transparentObject = data.getSortedTransparentRenderableObjects();
         if (clippingFrustum.has_value())
             QSSGLayerRenderData::frustumCulling(clippingFrustum.value(), transparentObject, sortedTransparentObjects);

tests/baseline/data/camera/FrustumCulling.qml
@@ -1,0 +1,89 @@
+// Copyright (C) 2023 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR BSD-3-Clause
+
+import QtQuick3D
+import QtQuick
+import QtQuick3D.Lancelot
+
+Rectangle {
+    id: perspective_camera
+    width: 800
+    height: 480
+    color: Qt.rgba(0, 0, 0, 1)
+
+    Node {
+        id: scene
+
+        DirectionalLight {
+            id: light
+            shadowFactor: 10
+        }
+    }
+
+    View3D {
+        id: leftView
+        anchors.left: parent.left
+        anchors.top: parent.top
+        width: parent.width * 0.5
+        height: parent.height * 1
+        environment: SceneEnvironment {
+            clearColor: Qt.rgba(0, 0, 0, 1)
+        }
+
+        camera: PerspectiveCamera {
+            position: Qt.vector3d(0, 0, 3)
+            clipNear: 1
+            clipFar: 3
+            frustumCullingEnabled: false
+        }
+
+        Model {
+            geometry: DynamicGeometry {
+                id: geometry
+
+            }
+            materials: [
+                PrincipledMaterial {
+                    baseColor: "green"
+                    lighting: PrincipledMaterial.NoLighting
+                }
+            ]
+        }
+
+        importScene: scene
+    }
+
+    View3D {
+        id: rightView
+        anchors.left: leftView.right
+        anchors.top: parent.top
+        anchors.right: parent.right
+        height: parent.height * 1
+        environment: SceneEnvironment {
+            clearColor: Qt.rgba(0, 0, 0, 1)
+        }
+
+        camera: PerspectiveCamera {
+            position: Qt.vector3d(0, 0, 3)
+            clipNear: 1
+            clipFar: 3
+            frustumCullingEnabled: true
+        }
+
+        importScene: scene
+
+        Model {
+            geometry: DynamicGeometry {
+                Component.onCompleted: changeBounds()
+
+            }
+            materials: [
+                PrincipledMaterial {
+                    baseColor: "red"
+                    lighting: PrincipledMaterial.NoLighting
+                }
+            ]
+        }
+
+    }
+}

tests/baseline/scenegrabber/dynamicgeometry.cpp
@@ -43,6 +43,11 @@ void DynamicGeometry::changeGeometry()
     setBounds(QVector3D(-2.0f, -1.0f, 0.0f), QVector3D(2.0f, 1.0f, 0.0f));
 }
 
+void DynamicGeometry::changeBounds()
+{
+    setBounds(QVector3D(-1.0f, -1.0f, -2.0f), QVector3D(1.0f, 1.0f, -1.0f));
+}
+
 void DynamicGeometry::updateData()
 {
     QByteArray vertexData;

tests/baseline/scenegrabber/dynamicgeometry.h
@@ -15,6 +15,7 @@ public:
 
 public Q_SLOTS:
     void changeGeometry();
+    void changeBounds();
 
 private:
     void updateData();