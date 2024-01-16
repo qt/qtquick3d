97424750e34f40baca230fe2e2610b6ac956f4c6
Laszlo Agocs <laszlo.agocs@qt.io>
2024-01-16 17:52:06 +0100

    Make OpenXR test fail if the header version is too old
    1.0.29 is picked somewhat randomly: the current Oculus
    mobile SDK contains 1.0.30, the upstream latest is 1.0.33.
    1.0.29 is from August 2023 and should contain the anchor
    and spatial entity stuff we use as those are introduced
    around 1.0.28.
    
    Change-Id: I3d0f4863a4d15052f6bdea5d2908a7a1ab5fc9a6
    Reviewed-by: Christian Strømme <christian.stromme@qt.io>

--- config.tests/quick3dxr_openxr/main.cpp
+++ config.tests/quick3dxr_openxr/main.cpp
@@ -3,6 +3,10 @@
 
 #include <openxr/openxr.h>
 
+#if XR_CURRENT_API_VERSION < XR_MAKE_VERSION(1, 0, 29)
+#error openxr.h is too old, needs >= 1.0.29
+#endif
+
 int main(void)
 {
     uint32_t extensionCount = 0;
