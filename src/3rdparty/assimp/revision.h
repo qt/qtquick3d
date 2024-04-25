3bdf25acecc999cefc8f1899f268c267cb84d66a
Andy Nichols <nezticle@gmail.com>
2024-04-25 13:15:03 +0200

    Update Assimp to v5.4.0
    Fixes: QTBUG-124760
    Pick-to: 6.7
    Change-Id: Ib68a63c5f8761faef38d6abb34aef1645ee799d3
    Reviewed-by: Christian Strømme <christian.stromme@qt.io>

--- src/3rdparty/assimp/revision.h
+++ src/3rdparty/assimp/revision.h
@@ -1,12 +1,12 @@
 #ifndef ASSIMP_REVISION_H_INC
 #define ASSIMP_REVISION_H_INC
 
-#define GitVersion 0xc60a259f
+#define GitVersion 0x8b9ed34e
 #define GitBranch "HEAD"
 
 #define VER_MAJOR 5
-#define VER_MINOR 3
-#define VER_PATCH 1
+#define VER_MINOR 4
+#define VER_PATCH 0
 #define VER_BUILD 0
 
 #define STR_HELP(x) #x
