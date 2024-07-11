bc5d632ad6fc9281fb11f22b9b5a6467cb74fd9a
Inho Lee <inho.lee@qt.io>
2024-07-11 14:37:40 +0200

    Skin: change a condition to use skinning for instancing
    When using BONE_TRANSFORMS or BONE_NORMAL_TRANSFORMS
    in CustomMaterial, skinning animation is supposed to be
    applied by the user. However, for instacing the matrix
    transform should be changed regardless of the usage.
    
    Change-Id: I7f28ac78d47ec15f9d8ac15bf6bd62d0b06d1a18
    Reviewed-by: Paul Olav Tvete <paul.tvete@qt.io>
    (cherry picked from commit 13e75897dda3d9fe8479b59c8d9d04420070abc8)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 3cce997a9ba1b4db299f8bb8d1d548bdc0931814)
    (cherry picked from commit 2c380a59824a0370323a53c9a998ce39abef3d46)

--- src/runtimerender/rendererimpl/qssgvertexpipelineimpl.cpp
+++ src/runtimerender/rendererimpl/qssgvertexpipelineimpl.cpp
@@ -181,7 +181,6 @@ void QSSGMaterialVertexPipeline::beginVertexGeneration(const QSSGShaderDefaultMa
             if (materialAdapter->usesCustomSkinning()) {
                 vertexShader.addInclude("skinanim.glsllib");
                 vertexShader.addUniform("qt_boneTexture", "sampler2D");
-                m_hasSkinning = false;
             }
 
             if (!materialAdapter->isUnshaded()) {
@@ -310,7 +309,7 @@ void QSSGMaterialVertexPipeline::beginVertexGeneration(const QSSGShaderDefaultMa
         if (m_hasMorphing && !hasCustomVertexShader)
             vertexShader.append("    qt_vertPosition.xyz = qt_getMorphPosition(qt_vertPosition.xyz);");
 
-        if (m_hasSkinning) {
+        if (m_hasSkinning && !materialAdapter->usesCustomSkinning()) {
             vertexShader.append("    mat4 skinMat = mat4(1);");
             vertexShader.append("    if (qt_vertWeights != vec4(0.0)) {");
             vertexShader.append("        skinMat = qt_getSkinMatrix(qt_vertJoints, qt_vertWeights);");
