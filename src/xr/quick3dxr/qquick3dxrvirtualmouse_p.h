305e93da0984d615fbaf7fd39ec198a85b832ed2
Elias Hautala <elias.hautala@qt.io>
2024-07-29 16:34:20 +0300

    Add scrolling to XrVirtualMouse
    Adds scrolling using properties scrollWheelX and scrollWheelY.
    
    Pick-to: 6.8
    Change-Id: Ifd4defcc6a5cf8eb5d765360dfbeb12bcd1ca1f5
    Reviewed-by: Nicholas Bennett <nicholas.bennett@qt.io>

--- src/xr/quick3dxr/qquick3dxrvirtualmouse_p.h
+++ src/xr/quick3dxr/qquick3dxrvirtualmouse_p.h
@@ -18,6 +18,7 @@
 #include <QObject>
 #include <QEvent>
 #include <QtQml/qqml.h>
+#include <QTimer>
 
 QT_BEGIN_NAMESPACE
 
@@ -30,6 +31,10 @@ class QQuick3DXrVirtualMouse : public QObject
     Q_PROPERTY(bool rightMouseButton READ rightMouseButton WRITE setRightMouseButton NOTIFY rightMouseButtonChanged)
     Q_PROPERTY(bool leftMouseButton READ leftMouseButton WRITE setLeftMouseButton NOTIFY leftMouseButtonChanged)
     Q_PROPERTY(bool middleMouseButton READ middleMouseButton WRITE setMiddleMouseButton NOTIFY middleMouseButtonChanged)
+    Q_PROPERTY(float scrollWheelX READ scrollWheelX WRITE setScrollWheelX NOTIFY scrollWheelXChanged)
+    Q_PROPERTY(float scrollWheelY READ scrollWheelY WRITE setScrollWheelY NOTIFY scrollWheelYChanged)
+    Q_PROPERTY(int scrollTimerInterval READ scrollTimerInterval WRITE setScrollTimerInterval NOTIFY scrollTimerIntervalChanged)
+    Q_PROPERTY(int scrollPixelDelta READ scrollPixelDelta WRITE setScrollPixelDelta NOTIFY scrollPixelDeltaChanged)
     Q_PROPERTY(QQuick3DNode* source READ source WRITE setSource NOTIFY sourceChanged)
     Q_PROPERTY(QQuick3DXrView* view READ view WRITE setView NOTIFY viewChanged)
     Q_PROPERTY(bool enabled READ enabled WRITE setEnabled NOTIFY enabledChanged)
@@ -43,6 +48,10 @@ public:
     bool rightMouseButton() const;
     bool leftMouseButton() const;
     bool middleMouseButton() const;
+    float scrollWheelX() const;
+    float scrollWheelY() const;
+    int scrollTimerInterval() const;
+    int scrollPixelDelta() const;
 
     QQuick3DNode *source() const;
     QQuick3DXrView* view() const;
@@ -53,17 +62,26 @@ public Q_SLOTS:
     void setRightMouseButton(bool rightMouseButton);
     void setLeftMouseButton(bool leftMouseButton);
     void setMiddleMouseButton(bool middleMouseButton);
+    void setScrollWheelX(float scrollWheelX);
+    void setScrollWheelY(float scrollWheelY);
+    void setScrollTimerInterval(int scrollTimerInterval);
+    void setScrollPixelDelta(int scrollPixelDelta);
     void setSource(QQuick3DNode* source);
     void setView(QQuick3DXrView* view);
     void setEnabled(bool enabled);
 
 private Q_SLOTS:
     void moveEvent();
+    void generateWheelEvent();
 
 Q_SIGNALS:
     void rightMouseButtonChanged(bool rightMouseButton);
     void leftMouseButtonChanged(bool leftMouseButton);
     void middleMouseButtonChanged(bool middleMouseButton);
+    void scrollWheelXChanged(float scrollWheelX);
+    void scrollWheelYChanged(float scrollWheelY);
+    void scrollTimerIntervalChanged(int scrollTimerInterval);
+    void scrollPixelDeltaChanged(int scrollPixelDelta);
     void sourceChanged(QQuick3DNode* source);
     void viewChanged(QQuick3DXrView* view);
     void enabledChanged(bool enabled);
@@ -71,9 +89,14 @@ Q_SIGNALS:
 private:
     void generateEvent(QEvent::Type type, Qt::MouseButton button = Qt::NoButton);
 
+    QTimer* m_scrollTimer = nullptr;
     bool m_rightMouseButton = false;
     bool m_leftMouseButton = false;
     bool m_middleMouseButton = false;
+    float m_scrollWheelX = 0;
+    float m_scrollWheelY = 0;
+    int m_scrollTimerInterval = 30;
+    int m_scrollPixelDelta = 15;
     QQuick3DNode* m_source = nullptr;
     QQuick3DXrView* m_view = nullptr;
     bool m_enabled = true;
