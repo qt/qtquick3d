57826edce00218153bc0067c85e3009eb994450d
Jonas Karlsson <jonas.karlsson@qt.io>
2024-08-08 10:48:24 +0200

    Remove point light pivot calculation
    It results in artifacts in the shadow map and according to the
    documentation, Node::pivot is used when applying rotations to the
    node and since the point light is supposed to ignore rotations we
    can remove the pivot from the calculation.
    
    Pick-to: 5.15
    Change-Id: I65cd31ff5678163ab1a6d83e5db164ed5c1006f3
    Reviewed-by: Laszlo Agocs <laszlo.agocs@qt.io>
    (cherry picked from commit df36ce09b6b67284ffc3305525b9eb58f83d887c)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 573f23e46931069c1d541cfe3cf3fd7532f22d01)
    (cherry picked from commit bf513ed28a0379a8ddad52ba82bf10a2df458679)
    Reviewed-by: Jonas Karlsson <jonas.karlsson@qt.io>

--- src/runtimerender/rendererimpl/qssgrenderer.cpp
+++ src/runtimerender/rendererimpl/qssgrenderer.cpp
@@ -854,7 +854,7 @@ static void setupCameraForShadowMap(const QSSGRenderCamera &inCamera,
         theCamera.clipFar = 0.5f * finalDims.z();
         theCamera.localTransform = QSSGRenderNode::calculateTransformMatrix(center, QSSGRenderNode::initScale, inLightPivot, QQuaternion::fromDirection(forward, up));
     } else if (inLight->type == QSSGRenderLight::Type::PointLight) {
-        theCamera.lookAt(inLightPos, QVector3D(0, 1.0, 0), QVector3D(0, 0, 0), inLightPivot);
+        theCamera.lookAt(inLightPos, QVector3D(0, 1.0, 0), QVector3D(0, 0, 0), QVector3D(0, 0, 0));
     }
 
     theCamera.calculateGlobalVariables(theViewport);
@@ -972,14 +972,14 @@ static void setupCubeShadowCameras(const QSSGRenderLight *inLight, QSSGRenderCam
                                          };
 
     const QVector3D inLightPos = inLight->getGlobalPos();
-    const QVector3D inLightPivot = inLight->pivot;
+    const QVector3D lightPivot = QVector3D(0, 0, 0);
 
     for (int i = 0; i < 6; ++i) {
         inCameras[i].parent = nullptr;
         inCameras[i].clipNear = 1.0f;
         inCameras[i].clipFar = qMax<float>(2.0f, inLight->m_shadowMapFar);
         inCameras[i].fov = qDegreesToRadians(90.f);
-        inCameras[i].localTransform = QSSGRenderNode::calculateTransformMatrix(inLightPos, QSSGRenderNode::initScale, inLightPivot, rotOfs[i]);
+        inCameras[i].localTransform = QSSGRenderNode::calculateTransformMatrix(inLightPos, QSSGRenderNode::initScale, lightPivot, rotOfs[i]);
         inCameras[i].calculateGlobalVariables(theViewport);
     }
 
