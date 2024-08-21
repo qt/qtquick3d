31adb87909f3edd1df97ea2a128474550bf37a93
Christian Strømme <christian.stromme@qt.io>
2024-08-21 14:38:46 +0200

    Remove the generationId from the texture data
    The generationId is basically just a counter to check how "dirty" a
    texture data instance is, and if it's dirty its rhi texture gets
    released and replaced. Instead of having this separate dirty tracking we
    now include the state in the hash and the size in the cache key, so any
    incompatible changes will trigger a new entry to be added and a new
    texture created. The old one will be released automatically later
    if not used.
    
    Change-Id: I289a8ad135121b4313d90f073d5ee64a7bcbed51
    Reviewed-by: Paul Olav Tvete <paul.tvete@qt.io>
    Reviewed-by: Inho Lee <inho.lee@qt.io>
    Reviewed-by: Andy Nichols <andy.nichols@qt.io>
    (cherry picked from commit fd9e4834da57478dd31d387f568d40a3d22aa72f)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 8e1ab9609123d6e8b4e78979306b794e6822933c)
    (cherry picked from commit a26401cf9a0f6f8d48a5aa23c902ad4edb30fc2e)
    Reviewed-by: Christian Strømme <christian.stromme@qt.io>

--- src/runtimerender/resourcemanager/qssgrenderbuffermanager.cpp
+++ src/runtimerender/resourcemanager/qssgrenderbuffermanager.cpp
@@ -232,24 +232,20 @@ QSSGRenderImageTexture QSSGBufferManager::loadRenderImage(const QSSGRenderImage 
 
 QSSGRenderImageTexture QSSGBufferManager::loadTextureData(QSSGRenderTextureData *data, MipMode inMipMode)
 {
-    const CustomImageCacheKey imageKey = { data, inMipMode };
+    QSSG_ASSERT(data != nullptr, return {});
+
+    const CustomImageCacheKey imageKey = { data, data->size(), inMipMode };
     auto theImageData = customTextureMap.find(imageKey);
     if (theImageData == customTextureMap.end()) {
         theImageData = customTextureMap.insert(imageKey, ImageData());
-    } else if (data->generationId() != theImageData->generationId) {
-        auto &renderImageTexture = theImageData.value().renderImageTexture;
-        if (toRhiFormat(data->format()) != renderImageTexture.m_texture->format()
-                || data->size() != renderImageTexture.m_texture->pixelSize()) {
-            // release first
-            releaseTextureData(imageKey);
-            // reinsert the placeholder since releaseTextureData removed from map
-            theImageData = customTextureMap.insert(imageKey, ImageData());
-        }
-        theImageData->generationId = data->generationId();
-    } else {
+    } else if (data->version() == theImageData->version) {
         // Return the currently loaded texture
         theImageData.value().usageCounts[currentLayer]++;
         return theImageData.value().renderImageTexture;
+    } else {
+        // Optimization: If only the version number has changed, we can attempt to reuse the texture.
+        // Just update the version number and let setRhiTexture handle the rest.
+        theImageData->version = data->version();
     }
 
     // Load the texture
@@ -261,10 +257,8 @@ QSSGRenderImageTexture QSSGBufferManager::loadTextureData(QSSGRenderTextureData 
         bool wasTextureCreated = false;
 
         if (setRhiTexture(theImageData.value().renderImageTexture, theLoadedTexture.data(), inMipMode, {}, data->debugObjectName, &wasTextureCreated)) {
-            if (wasTextureCreated) {
-                theImageData.value().generationId = data->generationId();
+            if (wasTextureCreated)
                 increaseMemoryStat(theImageData.value().renderImageTexture.m_texture);
-            }
         } else {
             theImageData.value() = ImageData();
         }
@@ -1991,6 +1985,14 @@ void QSSGBufferManager::decreaseMemoryStat(QSSGRenderMesh *mesh)
             + bufferMemorySize(mesh->subsets.at(0).rhi.indexBuffer);
     stats.meshDataSize = qMax(0u, stats.meshDataSize - s);
     m_contextInterface->rhiContext()->stats().meshDataSizeChanges(stats.meshDataSize);
+}
+
+size_t qHash(const QSSGBufferManager::CustomImageCacheKey &k, size_t seed) noexcept
+{
+    // NOTE: The data pointer should never be null, as null data pointers shouldn't be inserted into
+    //       the cached (make sure to check that before inserting!!!)
+    using MipMap_t = std::underlying_type_t<QSSGBufferManager::MipMode>;
+    return qHash(*k.data, seed) ^ MipMap_t(k.mipMode);
 }
 
 QT_END_NAMESPACE
