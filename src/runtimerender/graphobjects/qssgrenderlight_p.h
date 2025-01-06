bb3783f4c6785049fa44b894f607279eb84c1337
Christian Strømme <christian.stromme@qt.io>
2025-01-06 13:57:01 +0100

    Fix uninitialized members in QSSGRenderLight
    Pick-to: 6.5
    Coverity-Id: 471568
    Change-Id: I85d53ee454aed484515410c809b9fa32d82f9afe
    Reviewed-by: Andy Nichols <andy.nichols@qt.io>
    (cherry picked from commit bb490d9272bf38dc91efc2504f46553ed53b2b7a)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 0778b294a2a4f2b64efeab1954ebff5ce54c4a5e)

--- src/runtimerender/graphobjects/qssgrenderlight_p.h
+++ src/runtimerender/graphobjects/qssgrenderlight_p.h
@@ -68,8 +68,8 @@ struct Q_QUICK3DRUNTIMERENDER_EXPORT QSSGRenderLight : public QSSGRenderNode
 
     float m_pcfFactor = 2.0f;
 
-    bool m_bakingEnabled;
-    bool m_fullyBaked; // direct+indirect
+    bool m_bakingEnabled = false;
+    bool m_fullyBaked = false; // direct+indirect
 
     // Cascading shadow map options
     float m_csmSplit1 = 0.0f;
