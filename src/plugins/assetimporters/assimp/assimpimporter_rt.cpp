06959cb131598d075e4b21e8e2b4d206d88dc23c
Inho Lee <inho.lee@qt.io>
2025-05-28 14:28:39 +0200

    asset: don't change normals in assets
    Some assets' normal values might be changed in assimp's
    postprocessing and it could make unexpected results.
    
    It can be added by options later, if it is necessary.
    
    Fixes: QTBUG-137182
    Change-Id: I0c9206aae0986a84e1373db7a47677d797874b92
    Reviewed-by: Christian Strømme <christian.stromme@qt.io>
    (cherry picked from commit 41f2fcd036671d2a530dc8d836b4ff0d0b5041a0)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit e35ecc97fc32b78e846e889e119fbbff87295a57)
    (cherry picked from commit 56662f0a6e8db30c3111188ffa98e2e204f60d71)

--- src/plugins/assetimporters/assimp/assimpimporter_rt.cpp
+++ src/plugins/assetimporters/assimp/assimpimporter_rt.cpp
@@ -1518,7 +1518,6 @@ static qreal getRealOption(const QString &optionName, const QJsonObject &options
 
 #define demonPostProcessPresets ( \
     aiProcess_CalcTangentSpace              |  \
-    aiProcess_GenSmoothNormals              |  \
     aiProcess_JoinIdenticalVertices         |  \
     aiProcess_ImproveCacheLocality          |  \
     aiProcess_RemoveRedundantMaterials      |  \
