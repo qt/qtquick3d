qt/qtquick3d

commit 4d066e99bdc43ff96513343f5f335a3676277c8d

Remove the bone texture from the model context
Pick-to: 6.6
Change-Id: Ifd471fcb442a0cb29f27960926630d298460109c
Reviewed-by: Andy Nichols <andy.nichols@qt.io>

src/runtimerender/rendererimpl/qssglayerrenderdata.cpp
@@ -1036,6 +1036,22 @@ QRhiTexture *QSSGLayerRenderData::getLightmapTexture(const QSSGModelContext &mod
     return ret;
 }
 
+void QSSGLayerRenderData::setBonemapTexture(const QSSGModelContext &modelContext, QRhiTexture *bonemapTexture)
+{
+    bonemapTextures[&modelContext] = bonemapTexture;
+}
+
+QRhiTexture *QSSGLayerRenderData::getBonemapTexture(const QSSGModelContext &modelContext) const
+{
+    QRhiTexture *ret = nullptr;
+    if (modelContext.model.usesBoneTexture()) {
+        const auto it = bonemapTextures.constFind(&modelContext);
+        ret = (it != bonemapTextures.cend()) ? *it : nullptr;
+    }
+
+    return ret;
+}
+
 // inModel is const to emphasize the fact that its members cannot be written
 // here: in case there is a scene shared between multiple View3Ds in different
 // QQuickWindows, each window may run this in their own render thread, while
@@ -1070,12 +1086,12 @@ bool QSSGLayerRenderData::prepareModelsForRender(const RenderableNodeEntries &re
         // Prepare boneTexture for skinning
         if (model.skin) {
             auto boneTexture = bufferManager->loadSkinmap(model.skin);
-            theModelContext.boneTexture = boneTexture.m_texture;
+            setBonemapTexture(theModelContext, boneTexture.m_texture);
         } else if (model.skeleton) {
             auto boneTexture = bufferManager->loadSkinmap(&(model.skeleton->boneTexData));
-            theModelContext.boneTexture = boneTexture.m_texture;
+            setBonemapTexture(theModelContext, boneTexture.m_texture);
         } else {
-            theModelContext.boneTexture = nullptr;
+            setBonemapTexture(theModelContext, nullptr);
         }
 
         // many renderableFlags are the same for all the subsets
@@ -2146,6 +2162,7 @@ void QSSGLayerRenderData::resetForFrame()
     renderedBakedLightingModels.clear();
     renderableItem2Ds.clear();
     lightmapTextures.clear();
+    bonemapTextures.clear();
     globalLights.clear();
     modelContexts.clear();
     features = QSSGShaderFeatures();

src/runtimerender/graphobjects/qssgrendermodel_p.h
@@ -66,6 +66,7 @@ struct Q_QUICK3DRUNTIMERENDER_EXPORT QSSGRenderModel : public QSSGRenderNode
     QString lightmapLoadPath;
     uint lightmapBaseResolution = 0;
     bool hasLightmap() const { return !lightmapKey.isEmpty(); }
+    bool usesBoneTexture() const { return ((skin != nullptr) || (skeleton != nullptr)); }
 
     float levelOfDetailBias = 1.0f; // values < 1.0 will decrease usage of LODs, values > 1.0 will increase usage of LODs
 

src/runtimerender/qssgrhicustommaterialsystem.cpp
@@ -138,7 +138,9 @@ void QSSGCustomMaterialSystem::updateUniformsForCustomMaterial(QSSGRhiShaderPipe
     const auto &modelNode = renderable.modelContext.model;
     const QMatrix4x4 &localInstanceTransform(modelNode.localInstanceTransform);
     const QMatrix4x4 &globalInstanceTransform(modelNode.globalInstanceTransform);
-    const QMatrix4x4 &modelMatrix((renderable.modelContext.boneTexture) ? QMatrix4x4() : renderable.globalTransform);
+
+
+    const QMatrix4x4 &modelMatrix(modelNode.usesBoneTexture() ? QMatrix4x4() : renderable.globalTransform);
 
     QSSGMaterialShaderGenerator::setRhiMaterialProperties(*context,
                                                           shaderPipeline,
@@ -291,7 +293,7 @@ void QSSGCustomMaterialSystem::rhiPrepareRenderable(QSSGRhiGraphicsPipelineState
             samplerBindingsSpecified.setBit(shaderPipeline->bindingForTexture("qt_particleTexture"));
 
         // Skinning
-        if (renderable.modelContext.boneTexture) {
+        if (QRhiTexture *boneTexture = layerData.getBonemapTexture(renderable.modelContext)) {
             int binding = shaderPipeline->bindingForTexture("qt_boneTexture");
             if (binding >= 0) {
                 QRhiSampler *boneSampler = rhiCtx->sampler({ QRhiSampler::Nearest,
@@ -303,7 +305,7 @@ void QSSGCustomMaterialSystem::rhiPrepareRenderable(QSSGRhiGraphicsPipelineState
                                                            });
                 bindings.addTexture(binding,
                                     QRhiShaderResourceBinding::VertexStage,
-                                    renderable.modelContext.boneTexture,
+                                    boneTexture,
                                     boneSampler);
                 samplerBindingsSpecified.setBit(binding);
             }

src/runtimerender/rendererimpl/qssglayerrenderdata_p.h
@@ -320,6 +320,9 @@ public:
     void setLightmapTexture(const QSSGModelContext &modelContext, QRhiTexture *lightmapTexture);
     [[nodiscard]] QRhiTexture *getLightmapTexture(const QSSGModelContext &modelContext) const;
 
+    void setBonemapTexture(const QSSGModelContext &modelContext, QRhiTexture *bonemapTexture);
+    [[nodiscard]] QRhiTexture *getBonemapTexture(const QSSGModelContext &modelContext) const;
+
     [[nodiscard]] QSSGRenderContextInterface *contextInterface() const;
 
     // Temp. API. Ideally there shouldn't be a reason for anyone to hold onto these,
@@ -364,6 +367,7 @@ private:
     QSSGRenderShadowMapPtr shadowMapManager;
     QSSGRenderReflectionMapPtr reflectionMapManager;
     QHash<const QSSGModelContext *, QRhiTexture *> lightmapTextures;
+    QHash<const QSSGModelContext *, QRhiTexture *> bonemapTextures;
 };
 
 QT_END_NAMESPACE

src/runtimerender/rendererimpl/qssgrenderableobjects_p.h
@@ -272,13 +272,12 @@ struct QSSGModelContext
     const QSSGRenderModel &model;
     QMatrix4x4 modelViewProjection;
     QMatrix3x3 normalMatrix;
-    QRhiTexture *boneTexture = nullptr;
 
     QSSGModelContext(const QSSGRenderModel &inModel, const QMatrix4x4 &inViewProjection) : model(inModel)
     {
         // For skinning, node's global transformation will be ignored and
         // an identity matrix will be used for the normalMatrix
-        if (model.skin || model.skeleton) {
+        if (model.usesBoneTexture()) {
             modelViewProjection = inViewProjection;
             normalMatrix = QMatrix3x3();
         } else {

src/runtimerender/rendererimpl/qssgrenderer.cpp
@@ -86,7 +86,7 @@ static void updateUniformsForDefaultMaterial(QSSGRhiShaderPipeline &shaderPipeli
 
     const QMatrix4x4 &localInstanceTransform(modelNode.localInstanceTransform);
     const QMatrix4x4 &globalInstanceTransform(modelNode.globalInstanceTransform);
-    const QMatrix4x4 &modelMatrix((subsetRenderable.modelContext.boneTexture) ? QMatrix4x4() : subsetRenderable.globalTransform);
+    const QMatrix4x4 &modelMatrix(modelNode.usesBoneTexture() ? QMatrix4x4() : subsetRenderable.globalTransform);
 
     QSSGMaterialShaderGenerator::setRhiMaterialProperties(*renderer->contextInterface(),
                                                           shaderPipeline,
@@ -1306,7 +1306,7 @@ void RenderHelpers::rhiPrepareRenderable(QSSGRhiContext *rhiCtx,
                 QSSGParticleRenderer::prepareParticlesForModel(*shaderPipeline, rhiCtx, bindings, &subsetRenderable.modelContext.model);
 
             // Skinning
-            if (subsetRenderable.modelContext.boneTexture) {
+            if (QRhiTexture *boneTexture = inData.getBonemapTexture(subsetRenderable.modelContext)) {
                 int binding = shaderPipeline->bindingForTexture("qt_boneTexture");
                 if (binding >= 0) {
                     QRhiSampler *boneSampler = rhiCtx->sampler({ QRhiSampler::Nearest,
@@ -1318,7 +1318,7 @@ void RenderHelpers::rhiPrepareRenderable(QSSGRhiContext *rhiCtx,
                     });
                     bindings.addTexture(binding,
                                         QRhiShaderResourceBinding::VertexStage,
-                                        subsetRenderable.modelContext.boneTexture,
+                                        boneTexture,
                                         boneSampler);
                 }
             }

commit 09a1ae34f0de9a9535c35cc324bca2433fe97438

Don't insert null bone texture for each model context
This was clearly a mistake introduced in 4d066e99bdc43ff96513343f5f335a3676277c8d,
as the original code updated the pointer on the model
context regardless, and without cost. Since we now
store the texture in a map there's absolutely no good
reason why we should insert a null texture for all
model contexts.

Pick-to: 6.10 6.9 6.8
Change-Id: I7321218161e0037a24adbad6487819f54e59765d
Reviewed-by: Jonas Karlsson <jonas.karlsson@qt.io>

src/runtimerender/rendererimpl/qssglayerrenderdata.cpp
@@ -1604,8 +1604,6 @@ bool QSSGLayerRenderData::prepareModelsForRender(QSSGRenderContextInterface &con
         } else if (model.skeleton) {
             auto boneTexture = bufferManager->loadSkinmap(&(model.skeleton->boneTexData));
             setBonemapTexture(theModelContext, boneTexture.m_texture);
-        } else {
-            setBonemapTexture(theModelContext, nullptr);
         }
 
         // many renderableFlags are the same for all the subsets