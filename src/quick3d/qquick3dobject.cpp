35d48ad37d2a071959a71b577aef7fd2b2d88b01
Christian Strømme <christian.stromme@qt.io>
2025-08-14 13:46:26 +0200

    Fix the Layers documentation
    Fixed broken links, incorrect naming, and tagging for the ContentLayer
    type
    
    Change-Id: If2195374eed17c8da89e4c73c4f49e89dcd8428c
    Reviewed-by: Nicholas Bennett <nicholas.bennett@qt.io>

--- src/quick3d/qquick3dobject.cpp
+++ src/quick3d/qquick3dobject.cpp
@@ -263,24 +263,34 @@ void QQuick3DObject::preSync()
 }
 
 /*!
-    \class QQuick3DContentLayer
-    \inmodule QtQuick3D
+    \qmltype ContentLayer
+    \inqmlmodule QtQuick3D
     \since 6.11
     \brief Provides enumeration of content layers available in a QtQuick3D.
 
     This class is used to represent a layer of content in a 3D scene. It can be used
     to manage different layers of content, such as background, foreground, or UI elements.
 
-    The \l ContentLayer enumeration defines the available layer flags that can be used to
-    for example group scene nodes togheter or filter which nodes are rendered by the camera.
+    The \l ContentLayer enumeration defines the available layer flags that can be used,
+    for example, to group scene nodes together or filter which nodes are rendered by the camera.
 
+    The predefined layer names are not necessarily descriptive enough for specific use cases, in
+    which case you can define your own layers by aliasing the \l ContentLayer enumeration values.
     Consider a city scene where you have different layers for buildings, streets, and vehicles.
-    You can assign each of these layers a specific flag, such as \c LayerBuildings, \c LayerStreets,
-    and \c LayerVehicles by aliasing the \l ContentLayer enumeration values. Once the layers are defined,
+    You can assign each of these layers a specific flag, such as \c layerBuildings, \c layerStreets,
+    and \c layerVehicles by aliasing the \l ContentLayer enumeration values. Once the layers are defined,
     you can assign these flags to the nodes in the scene and have the camera filter which layers to render
     by setting the same \l {Node::layer}{layer} property on the \l Camera.
 
-    \note That some of the layers have special meanings:
+    \qml
+    readonly property int layerBuildings: ContentLayer.Layer1
+    readonly property int layerStreets: ContentLayer.Layer2
+    readonly property int layerVehicles: ContentLayer.Layer3 | ContentLayer.Layer4
+    \endqml
+
+    \sa {Qt Quick 3D - Layers Example}{Layers Example}
+
+    \note Some of the layers have special meanings:
 
     \table
     \header
@@ -291,7 +301,7 @@ void QQuick3DObject::preSync()
         \li No layer assigned, used to indicate that a node does not belong to any layer and should not be rendered.
     \row
         \li Layer0
-        \li The main layer, used for the primary content in the scene.
+        \li The main layer, used for the primary content in the scene. Nodes are assigned to this layer by default.
     \row
         \li Layer1-23
         \li Freely assignable layers, used for grouping nodes together based on their purpose or functionality.
