eacd0dd3847253b7bcc3e7645d60b5661348cb3d
Inho Lee <inho.lee@qt.io>
2025-08-15 15:41:20 +0200

    Update the importScene's scenemanger when destroyed
    importScenes can share a sceneManager with other importScenes or
    sceneRoots. In that case, if the sceneManager was deleted,
    the importScenes' sceneManagers were not updated correctly which
    could result in the application crashing.
    
    Fixes: QTBUG-139232
    Pick-to: 6.10 6.9 6.8
    Change-Id: I018cecbd61cf39cfd18b7cc94759b636caecd5ae
    Reviewed-by: Christian Strømme <christian.stromme@qt.io>

--- src/quick3d/qquick3dviewport_p.h
+++ src/quick3d/qquick3dviewport_p.h
@@ -219,6 +219,7 @@ private:
     QQuick3DPickResult processPickResult(const QSSGRenderPickResult &pickResult) const;
     QQuick3DObject *findFrontendNode(const QSSGRenderGraphObject *backendObject) const;
     QQuick3DSceneManager *findChildSceneManager(QQuick3DObject *inObject, QQuick3DSceneManager *manager = nullptr);
+    void updateSceneManagerForImportScene();
     QQuick3DCamera *m_camera = nullptr;
     QVarLengthArray<QQuick3DCamera *, 2> m_multiViewCameras;
     QQuick3DSceneEnvironment *m_environment = nullptr;
