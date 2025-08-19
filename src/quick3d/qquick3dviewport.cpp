aa5d8b2449a6ccd9297105e84a4d199938538b52
Inho Lee <inho.lee@qt.io>
2025-08-19 13:44:33 +0200

    Use UniqueConnection between sceneManagers and viewports
    A sceneManager can be shared between variant 3d nodes
    so we should avoid connecting to it more then once.
    
    Pick-to: 6.8
    Change-Id: I08a44995d007fdff839291b8fc7db9fe3079af31
    Reviewed-by: Christian Strømme <christian.stromme@qt.io>
    (cherry picked from commit df45a900a3ecc7d0c87054534ec45b89643d3386)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 35a3394fcdb02c84ed8757ba102cc0dfd450bce8)

--- src/quick3d/qquick3dviewport.cpp
+++ src/quick3d/qquick3dviewport.cpp
@@ -2109,7 +2109,7 @@ void QQuick3DViewport::updateSceneManagerForImportScene()
 
     }
     connect(privateObject->sceneManager, &QQuick3DSceneManager::needsUpdate,
-            this, &QQuickItem::update);
+            this, &QQuickItem::update, Qt::UniqueConnection);
     connect(privateObject->sceneManager, &QObject::destroyed,
             this, [&](QObject *) {
                 auto privateObject = QQuick3DObjectPrivate::get(m_importScene);
@@ -2125,7 +2125,7 @@ void QQuick3DViewport::updateSceneManagerForImportScene()
         if (scene) {
             connect(QQuick3DObjectPrivate::get(scene)->sceneManager,
                     &QQuick3DSceneManager::needsUpdate,
-                    this, &QQuickItem::update);
+                    this, &QQuickItem::update, Qt::UniqueConnection);
         }
     }
 }
