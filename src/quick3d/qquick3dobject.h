48b298655f36ce8ca8f1d3eb47b44bc5c4248efd
Christian Strømme <christian.stromme@qt.io>
2025-12-22 14:21:17 +0100

    Move QQuick3ContentLayer class into its own class
    As per the API-review there's no real need for the ContentLayer to be in
    the QQuick3DObject's public header.
    
    Change-Id: I12505e66bf4445860eebbee15f4a5b93eed843d7
    Reviewed-by: Volker Hilsheimer <volker.hilsheimer@qt.io>
    (cherry picked from commit b0b5ed7bde5dff4511a2bdf0f41614dfb1fd8839)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/quick3d/qquick3dobject.h
+++ src/quick3d/qquick3dobject.h
@@ -108,59 +108,6 @@ private:
     friend class QQuick3DSceneManager;
 };
 
-class Q_QUICK3D_EXPORT QQuick3DContentLayer : public QObject
-{
-    Q_OBJECT
-    QML_NAMED_ELEMENT(ContentLayer)
-    QML_ADDED_IN_VERSION(6, 11)
-    QML_SINGLETON
-public:
-    explicit QQuick3DContentLayer(QObject *parent = nullptr);
-    ~QQuick3DContentLayer() override;
-
-    enum LayerFlag : quint32 {
-        LayerNone = 0x0,
-        Layer0 = 0x1, // Layer0 is reserved for the main layer.
-        Layer1 = 0x2,
-        Layer2 = 0x4,
-        Layer3 = 0x8,
-        Layer4 = 0x10,
-        Layer5 = 0x20,
-        Layer6 = 0x40,
-        Layer7 = 0x80,
-        Layer8 = 0x100,
-        Layer9 = 0x200,
-        Layer10 = 0x400,
-        Layer11 = 0x800,
-        Layer12 = 0x1000,
-        Layer13 = 0x2000,
-        Layer14 = 0x4000,
-        Layer15 = 0x8000,
-        Layer16 = 0x10000,
-        Layer17 = 0x20000,
-        Layer18 = 0x40000,
-        Layer19 = 0x80000,
-        Layer20 = 0x100000,
-        Layer21 = 0x200000,
-        Layer22 = 0x400000,
-        Layer23 = 0x800000,
-        // All layers from Layer1 to Layer23 are reserved for user-defined layers.
-        LayerAll = 0xFFFFFF,
-        // Layers 24 and above are reserved for internal usage.
-        Layer24 = 0x1000000,
-        Layer25 = 0x2000000,
-        Layer26 = 0x4000000,
-        Layer27 = 0x8000000,
-        Layer28 = 0x10000000,
-        Layer29 = 0x20000000,
-        Layer30 = 0x40000000,
-        Layer31 = 0x80000000,
-        ReservedLayerMask = 0xFF000000
-    };
-    Q_DECLARE_FLAGS(LayerFlags, LayerFlag)
-    Q_FLAG(LayerFlags)
-};
-
 QT_END_NAMESPACE
 
 QML_DECLARE_TYPE(QQuick3DObject)
