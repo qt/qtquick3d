52ca0353477f1448567de270f0b20f3067b64cef
Christian Strømme <christian.stromme@qt.io>
2026-01-22 16:56:22 +0100

    Fix stack-use-after-scope in the scene renderer and ibl baker
    Two instances where holding on to a pointer to data inside a temporary
    instance of a QRhiTextureRenderTargetDescription.
    
    Pick-to: 6.8
    Change-Id: I96a39bec34cf54e24023d56f8b3991a44a017213
    Reviewed-by: Laszlo Agocs <laszlo.agocs@qt.io>
    (cherry picked from commit a80ee37c519850d2a90fa88116d482d3a1c2b214)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 2f76f26dda1ef7c7ae248bfa727ac67ae81f8644)

--- src/iblbaker/qssgiblbaker.cpp
+++ src/iblbaker/qssgiblbaker.cpp
@@ -544,7 +544,8 @@ QString renderToKTXFileInternal(const char *name, const QString &inPath, const Q
             // Read back texture
             Q_ASSERT(rhi->isRecordingFrame());
 
-            const auto texture = renderTarget->description().cbeginColorAttachments()->texture();
+            const auto descr = renderTarget->description();
+            const auto texture = descr.cbeginColorAttachments()->texture();
 
             QRhiReadbackResult result;
             QRhiReadbackDescription readbackDesc(texture); // null src == read from swapchain backbuffer
