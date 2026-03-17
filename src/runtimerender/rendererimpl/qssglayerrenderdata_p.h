b4c4f702a579d64aa38c7c5d3967b140b650067f
Christian Strømme <christian.stromme@qt.io>
2026-03-17 08:54:37 +0100

    Loosen up on requiring an explicit camera with layer mask
    Setting an explicit camera should be the preferred solution, but we've
    allowed for detection of "free" cameras since the beginning, so it's not
    an uncommon pattern unfortunately. To avoid paper cuts when using the
    layer mechanism, we now recover and print a warning when a non-explicit
    camera has a layer mask. A warning will be issued to make the user is
    aware that they are doing a sub-optimal thing in their code.
    
    Change-Id: I9a4d1e0b8b7598438bd6ca187f247d3e9519e0f7
    Reviewed-by: Jonas Karlsson <jonas.karlsson@qt.io>
    (cherry picked from commit e3d73f6e62f4f9f57742858e2d46140180a67f3e)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/runtimerender/rendererimpl/qssglayerrenderdata_p.h
+++ src/runtimerender/rendererimpl/qssglayerrenderdata_p.h
@@ -415,20 +415,36 @@ public:
 
     using LayerNodes = std::vector<QSSGRenderNode *>;
     QSSGGlobalRenderNodeData::LayerNodeView layerNodes;
-    LayerNodes layerNodesCategorized;
 
     // renderableNodes have all lights, but properties configured for specific node
     RenderableNodeEntries renderableModels;
     RenderableNodeEntries renderableParticles;
 
     // Views into the collected nodes (unsorted)
-    QSSGModelsView modelsView;
-    QSSGParticlesView particlesView;
-    QSSGItem2DsView item2DsView;
-    QSSGCamerasView camerasView;
-    QSSGLightsView lightsView;
-    QSSGReflectionProbesView reflectionProbesView;
-    QSSGNonCategorizedView nonCategorizedView;
+    class NodeCollection
+    {
+        Q_DISABLE_COPY(NodeCollection)
+    public:
+        NodeCollection() = default;
+
+        QSSGModelsView modelsView;
+        QSSGParticlesView particlesView;
+        QSSGItem2DsView item2DsView;
+        QSSGCamerasView camerasView;
+        QSSGLightsView lightsView;
+        QSSGReflectionProbesView reflectionProbesView;
+        QSSGNonCategorizedView nonCategorizedView;
+        LayerNodes layerNodesCategorized;
+    };
+    NodeCollection nodeCollection;
+    // FIXME: Convenience for now
+    QSSGModelsView &modelsView = nodeCollection.modelsView;
+    QSSGParticlesView &particlesView = nodeCollection.particlesView;
+    QSSGItem2DsView &item2DsView = nodeCollection.item2DsView;
+    QSSGCamerasView &camerasView = nodeCollection.camerasView;
+    QSSGLightsView &lightsView = nodeCollection.lightsView;
+    QSSGReflectionProbesView &reflectionProbesView = nodeCollection.reflectionProbesView;
+    QSSGNonCategorizedView &nonCategorizedView = nodeCollection.nonCategorizedView;
 
     // Results of prepare for render.
     QSSGRenderCameraList renderedCameras; // multiple items with multiview, one otherwise (or zero if no cameras at all)
@@ -447,6 +463,7 @@ public:
 
     QQsbCollection::EntryMap m_particleShaderEntries;
 
+    bool nonExplicitCameraWithLayerMaskWarningShown = false;
     bool tooManyLightsWarningShown = false;
     bool tooManyDirectionalLightsWarningShown = false;
     bool oitWarningUnsupportedShown = false;
@@ -656,6 +673,10 @@ public:
                                                                         bool anyLightHasShadows,
                                                                         QSSGLayerRenderPreparationResultFlags &ioFlags);
 
+    static void categorizeAndFilterNodes(const QSSGGlobalRenderNodeData::LayerNodeView &layerNodes,
+                                          QSSGLayerRenderData::NodeCollection &nodeCollection,
+                                          quint32 layerMask);
+    void updateFilteredLayerNodes(quint32 layerMask);
 private:
     friend class QSSGRenderer;
     friend class QSSGRendererPrivate;
