d095bc5b288d80a54f57ecb8a964b365214b871f
Christian Strømme <christian.stromme@qt.io>
2026-03-31 17:32:45 +0200

    Exclude screen texture renderables in the reflection map pass
    Exclude renderables that require a screen texture from participating in
    the reflection map pass. Originally this change was just intended for
    when multiview rendering was enabled, since that caused issues due to
    the reflection map being rendered as non-multiview. However, using the
    main screen texture in the reflection probe is just wrong and, while it
    does create a result, it's not going to be correct, so screen textures
    object are now completely removed from the reflection map pass. Since
    this is a behavior change, an environment variable to restore the
    previous behavior was added:
    QT_QUICK3D_REFLECTION_PASS_INCLUDE_SCREEN_TEXTURE_OBJECTS
    
    Pick-to: 6.11
    Change-Id: I742ce1e291ba9fd7652bc4048157c508d64cebcb
    Reviewed-by: Andy Nichols <andy.nichols@qt.io>

--- src/runtimerender/rendererimpl/qssgrenderpass_p.h
+++ src/runtimerender/rendererimpl/qssgrenderpass_p.h
@@ -103,6 +103,7 @@ public:
 class ReflectionMapPass : public QSSGRenderPass
 {
 public:
+    ReflectionMapPass();
     void renderPrep(QSSGRenderer &renderer, QSSGLayerRenderData &data) final;
     void renderPass(QSSGRenderer &renderer) final;
     Type passType() const final { return Type::Standalone; }
@@ -112,6 +113,7 @@ public:
     QList<QSSGRenderReflectionProbe *> reflectionProbes;
     QSSGRenderableObjectList reflectionPassObjects;
     QSSGRhiGraphicsPipelineState ps;
+    bool m_includeSTO = false; // Compatibility flag to include STO objects in reflection map rendering.
 };
 
 class ZPrePassPass : public QSSGRenderPass
