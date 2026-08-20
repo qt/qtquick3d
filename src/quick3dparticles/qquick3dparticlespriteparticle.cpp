f2fec4c89171edd8ce21464af8051dcd083982fa
Marc Mutz <marc.mutz@qt.io>
2026-08-20 12:47:22 +0200

    QQuick3DParticleSpriteParticle: port m_connections from QHash to std::array
    The QHash was only ever filled with the same three connections, and
    the QHash key was never read after insert().
    
    Replace the QHash with a std::array, and the insert()s with a
    mass-move, to statically assert that the size of the std::array and
    the number of connections are the same.
    
    This matches how other classes manage their connections.
    
    As a drive-by, remove unneeded cruft from the connect() calls:
    - QObject:: scope in a QObject subclass
    - empty () on lambdas
    - lambda use when a PMF can be used directly
    
    Amends 01ee811f87fc8e396de2032f3419d6a3581694e4 (6.1).
    
    Pick-to: 6.11 6.8
    Change-Id: I000bcd6cdcb458a81c0b2b276d6f15f4897f2858
    Reviewed-by: Christian Strømme <christian.stromme@qt.io>
    (cherry picked from commit 29c21f18c6e22dc1aabf873dc9e4bc23911ce568)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/quick3dparticles/qquick3dparticlespriteparticle.cpp
+++ src/quick3dparticles/qquick3dparticlespriteparticle.cpp
@@ -26,15 +26,16 @@ QT_BEGIN_NAMESPACE
 QQuick3DParticleSpriteParticle::QQuick3DParticleSpriteParticle(QQuick3DNode *parent)
     : QQuick3DParticle(parent)
 {
-    m_connections.insert("maxAmount", QObject::connect(this, &QQuick3DParticle::maxAmountChanged, this, [this]() {
-        handleMaxAmountChanged(m_maxAmount);
-    }));
-    m_connections.insert("system", QObject::connect(this, &QQuick3DParticle::systemChanged, this, [this]() {
-        handleSystemChanged(system());
-    }));
-    m_connections.insert("sortMode", QObject::connect(this, &QQuick3DParticle::sortModeChanged, this, [this]() {
-        markNodesDirty();
-    }));
+    m_connections = std::array{
+            connect(this, &QQuick3DParticle::maxAmountChanged, this, [this] {
+                handleMaxAmountChanged(m_maxAmount);
+            }),
+            connect(this, &QQuick3DParticle::systemChanged, this, [this] {
+                handleSystemChanged(system());
+            }),
+            connect(this, &QQuick3DParticle::sortModeChanged,
+                    this, &QQuick3DParticleSpriteParticle::markNodesDirty),
+    };
 }
 
 QQuick3DParticleSpriteParticle::~QQuick3DParticleSpriteParticle()
